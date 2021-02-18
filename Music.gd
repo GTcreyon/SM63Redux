cabd529a1a2819cd30de68835b92470b9b2c09d6
GTcreyon <plaidyoshi@gmail.com>
2021-02-18 21:40:45 +0000

    Movement and Music!
    +Editor music
    +Mario voice sfx
    +Spin
    +Functional start position in editor
    +Better dive mechanics

---
+++ Music.gd
@@ -0,0 +1,44 @@
+extends AudioStreamPlayer;
+
+onready var fadeOut = $FadeOut;
+onready var fadeIn = $FadeIn;
+
+const song1 = preload("res://Audio/Music/LD/editor1.ogg");
+const song2 = preload("res://Audio/Music/LD/editor2.ogg");
+const song3 = preload("res://Audio/Music/LD/editor3.ogg");
+const song4 = preload("res://Audio/Music/LD/editor4.ogg");
+
+const songList = [song1, song2, song3, song4];
+
+var songLength;
+var playLength;
+var lengthFactor;
+var songNum;
+
+func switch_song():
+	songNum = (songNum + 1 + (randi() % 3)) % 4;
+	stream = songList[songNum];
+	play(0);
+	fadeIn.interpolate_property(self, "volume_db", -80, 0, 5, 1, Tween.EASE_OUT);
+	fadeIn.start();
+	songLength = stream.get_length();
+	lengthFactor = rand_range(1, 3);
+	#lengthFactor = 0.3;
+	playLength = songLength * lengthFactor;
+	fadeOut.interpolate_property(self, "volume_db", 0, -80, 30, 1, Tween.EASE_IN, playLength);
+	fadeOut.start();
+#
+#func _process(delta):
+#	if get_playback_position() > playLength + 10:
+#
+
+func _ready():
+	randomize();
+	songNum = randi() % 4;
+	volume_db = -80;
+	switch_song();
+	
+func _on_FadeOut_tween_completed(object, key):
+	# stop the music -- otherwise it continues to run at silent volume
+	stop();
+	switch_song();
--- src/Player.gd
+++ src/Player.gd
@@ -20,10 +20,43 @@ const setDoubleJumpFrames = 17;
 const setTJumpDeadzone = 3.0 * fpsMod;
 const setDiveSpeed = 35.0 * fpsMod;
 
+onready var voice = $Voice;
+
+const voiceBank = {
+	"jump1": [
+		preload("res://Audio/SFX/Mario/Ma1.ogg"),
+		preload("res://Audio/SFX/Mario/Ma2.ogg"),
+		preload("res://Audio/SFX/Mario/Ma3.ogg"),
+		#preload("res://Audio/SFX/Mario/Ha4.ogg"),
+	],
+	"jump2": [
+		preload("res://Audio/SFX/Mario/Ya1.ogg"),
+		preload("res://Audio/SFX/Mario/Ya2.ogg"),
+		#preload("res://Audio/SFX/Mario/Ya3.ogg"),
+		preload("res://Audio/SFX/Mario/Ya4.ogg"),
+	],
+	"jump3": [
+		preload("res://Audio/SFX/Mario/Wahoo1.ogg"),
+		preload("res://Audio/SFX/Mario/Wahoo2.ogg"),
+	],
+	"dive": [
+		preload("res://Audio/SFX/Mario/Ha1.ogg"),
+		preload("res://Audio/SFX/Mario/Ha2.ogg"),
+		preload("res://Audio/SFX/Mario/Ha3.ogg"),
+		#preload("res://Audio/SFX/Mario/Ha4.ogg"),
+#		preload("res://Audio/SFX/Mario/Ya1.ogg"),
+#		preload("res://Audio/SFX/Mario/Ya2.ogg"),
+#		#preload("res://Audio/SFX/Mario/Ya3.ogg"),
+#		preload("res://Audio/SFX/Mario/Ya4.ogg"),
+	],
+};
+
 var jumpFrames = -1;
 var vel = Vector2();
 var doubleJumpState = 0;
 var doubleJumpFrames = 0;
+var spinTimer = 0;
+var diveFlipL;
 
 enum s {
 	Walk,
@@ -39,6 +72,12 @@ enum s {
 var state = s.Walk;
 var classic;
 
+func play_voice(groupName):
+	var group = voiceBank[groupName];
+	var sound = group[randi() % group.size()];
+	voice.stream = sound;
+	voice.play(0);
+
 func update_classic():
 	classic = $"/root/Main".classic;
 	if classic:
@@ -48,6 +87,7 @@ func update_classic():
 		
 func switch_state(newState):
 	state = newState;
+	$AnimatedSprite.rotation_degrees = 0;
 	match state:
 		s.Dive:
 			$StandHitbox.disabled = true;
@@ -73,9 +113,13 @@ var debugMaxYVel = 0;
 func _physics_process(delta):
 	var iLeft = Input.is_action_pressed("left");
 	var iRight = Input.is_action_pressed("right");
+	var iDown = Input.is_action_pressed("down");
 	var iJump = Input.is_action_just_pressed("jump");
 	var iJumpH = Input.is_action_pressed("jump");
-	var iDive = Input.is_action_pressed("dive");
+	var iDive = Input.is_action_just_pressed("dive");
+	var iDiveH = Input.is_action_pressed("dive");
+	var iSpin = Input.is_action_just_pressed("spin");
+	var iSpinH = Input.is_action_pressed("spin");
 	if (Input.is_action_just_pressed("debug")):
 		if iJumpH:
 			get_tree().change_scene("res://LevelDesigner.tscn");
@@ -88,7 +132,7 @@ func _physics_process(delta):
 	
 	var fallAdjust = vel.y; #Used to adjust downward acceleration to account for framerate differenc
 	if state == s.Diveflip:
-		if $AnimatedSprite.flip_h:
+		if diveFlipL:
 			$AnimatedSprite.rotation_degrees -= 20;
 		else:
 			$AnimatedSprite.rotation_degrees += 20;
@@ -130,6 +174,9 @@ func _physics_process(delta):
 		if fallAdjust > 0:
 			fallAdjust = ground_friction(fallAdjust, ((grav/fpsMod)/5), 1.05);
 		fallAdjust = ground_friction(fallAdjust, 0, 1.001);
+		if state == s.Spin && !iDown:
+			#fallAdjust = ground_friction(fallAdjust, 0.3, 1.05); #fastspin
+			fallAdjust = ground_friction(fallAdjust, 0.1, 1.03);
 		vel.x = ground_friction(vel.x, 0, 1.001); #Air friction
 		
 		jumpFrames = max(jumpFrames - 1, -1);
@@ -139,8 +186,10 @@ func _physics_process(delta):
 	if iJumpH:
 		if ground:
 			if state == s.Dive:
+				move_and_slide(Vector2(0, -4.5)*60, Vector2(0, -1));
 				switch_state(s.Diveflip);
 				$AnimatedSprite.animation = "jump";
+				diveFlipL = $AnimatedSprite.flip_h;
 				vel.y = min(-setJumpVel/1.5, vel.y);
 			elif iJump:
 				jumpFrames = setJumpModFrames;
@@ -148,19 +197,26 @@ func _physics_process(delta):
 				
 				match doubleJumpState:
 					0: #Single
+						switch_state(s.Walk);
+						play_voice("jump1");
 						vel.y = -setJumpVel;
 						doubleJumpState+=1;
 					1: #Double
+						switch_state(s.Walk);
+						play_voice("jump2");
 						vel.y = -setDJumpVel;
 						doubleJumpState+=1;
 					2: #Triple
+						
 						if abs(vel.x) > setTJumpDeadzone:
 							vel.y = -setTJumpVel;
 							vel.x += (vel.x + 15*fpsMod*sign(vel.x))/5*fpsMod;
 							doubleJumpState = 0;
 							switch_state(s.Frontflip);
+							play_voice("jump3");
 						else:
 							vel.y = -setDJumpVel; #Not moving left/right fast enough
+							play_voice("jump2");
 				
 				if !classic:
 					move_and_collide(Vector2(0, -setJumpTP)); #Suggested by Maker - slight upwards teleport
@@ -169,51 +225,66 @@ func _physics_process(delta):
 	
 	debugMaxYVel = max(debugMaxYVel, vel.y);
 	
-	if iLeft && state != s.Diveflip:
-		if state != s.Dive:
+	if iLeft && !iRight:
+		if state != s.Dive && (state != s.Diveflip || !classic):
 			$AnimatedSprite.flip_h = true;
 		if ground:
 			if state != s.Dive:
 				vel.x -= setWalkAccel;
 		else:
-			if state == s.Frontflip:
-				vel.x -= max((vel.x+setAirAccel)/(setAirSpeedCap/(3*fpsMod)), 0) / (1.5 / fpsMod);
-			elif state == s.Dive:
-				vel.x -= max((vel.x+setAirAccel)/(setAirSpeedCap/(3*fpsMod)), 0) / (8 / fpsMod);
+			if state == s.Frontflip || state == s.Spin:
+				vel.x -= max((setAirAccel+vel.x)/(setAirSpeedCap/(3*fpsMod)), 0) / (1.5 / fpsMod);
+			elif state == s.Dive || state == s.Diveflip:
+				vel.x -= max((setAirAccel+vel.x)/(setAirSpeedCap/(3*fpsMod)), 0) / (8 / fpsMod);
 			else:
-				vel.x -= max((vel.x+setAirAccel)/(setAirSpeedCap/(3*fpsMod)), 0);
+				vel.x -= max((setAirAccel+vel.x)/(setAirSpeedCap/(3*fpsMod)), 0);
 			
-	if iRight:
-		if state != s.Dive && state != s.Diveflip:
+	if iRight && !iLeft:
+		if state != s.Dive && (state != s.Diveflip || !classic):
 			$AnimatedSprite.flip_h = false;
 		if ground:
 			if state != s.Dive:
 				vel.x += setWalkAccel;
 		else:
-			if state == s.Frontflip:
-				vel.x -= min((vel.x-setAirAccel)/(setAirSpeedCap/(3*fpsMod)), 0) / (1.5 / fpsMod);
+			if state == s.Frontflip || state == s.Spin:
+				vel.x += max((setAirAccel-vel.x)/(setAirSpeedCap/(3*fpsMod)), 0) / (1.5 / fpsMod);
 			elif state == s.Dive || state == s.Diveflip:
-				vel.x -= min((vel.x-setAirAccel)/(setAirSpeedCap/(3*fpsMod)), 0) / (8 / fpsMod);
+				vel.x += max((setAirAccel-vel.x)/(setAirSpeedCap/(3*fpsMod)), 0) / (8 / fpsMod);
 			else:
-				vel.x -= min((vel.x-setAirAccel)/(setAirSpeedCap/(3*fpsMod)), 0);
-		
-	if iDive && state != s.Dive && state != s.Diveflip: #Dive
+				vel.x += max((setAirAccel-vel.x)/(setAirSpeedCap/(3*fpsMod)), 0);
+	
+	if iDiveH && state != s.Dive && (state != s.Diveflip || (!classic && iDive && $AnimatedSprite.flip_h != diveFlipL)) && state != s.Pound  && (state != s.Spin || (!classic && iDive)): #Dive
 		if ground && iJumpH:
+			move_and_slide(Vector2(0, -4.5)*60, Vector2(0, -1));
 			switch_state(s.Diveflip);
 			$AnimatedSprite.animation = "jump";
+			diveFlipL = $AnimatedSprite.flip_h;
 			vel.y = min(-setJumpVel/1.5, vel.y);
 		else:
 			switch_state(s.Dive);
 			$AnimatedSprite.animation = "dive";
 			doubleJumpState = 0;
-			move_and_collide(Vector2(0, 6));
-			if !ground:
+			if ground:
+				move_and_slide(Vector2(0, 4.5)*60, Vector2(0, -1));
+			else:
+				play_voice("dive");
 				if $AnimatedSprite.flip_h:
-					vel.x += (vel.x - setDiveSpeed) / (5 / fpsMod) / fpsMod;
+					vel.x -= (setDiveSpeed - abs(vel.x)) / (5 / fpsMod) / fpsMod;
 				else:
-					vel.x += (vel.x + setDiveSpeed) / (5 / fpsMod) / fpsMod;
+					vel.x += (setDiveSpeed - abs(vel.x)) / (5 / fpsMod) / fpsMod;
 				vel.y += 3.0 * fpsMod;
-			
+	
+	if state == s.Spin:
+		if spinTimer > 0:
+			spinTimer -= 1;
+		elif !iSpinH:
+			state = s.Walk;
+	
+	if iSpinH && state != s.Spin && state != s.Frontflip && state != s.Pound && state != s.Dive && (state != s.Diveflip || (!classic && iSpin)) && vel.y > -3.3 * fpsMod:
+		switch_state(s.Spin);
+		$AnimatedSprite.animation = "spin";
+		vel.y = min(-3.3 * fpsMod, vel.y - 3.3 * fpsMod);
+		spinTimer = 30;
 	
 	if wall:
 		if int(iRight) - int(iLeft) != sign(int(vel.x)) && int(vel.x) != 0:
