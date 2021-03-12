978100f801663111f20703bfe51da982aabbd81f
GTcreyon <plaidyoshi@gmail.com>
2021-03-12 20:12:54 +0000

    Updated coins

---
+++ actors/items/2/coin_script.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+func update_classic():
+	var classic = $"/root/Main".classic; #this isn't a filename don't change Main to lowercase lol
+	if classic:
+		$Sprite.animation = "legacy";
+	else:
+		$Sprite.animation = "modern";
+
+func _process(delta):
+	if Input.is_action_just_pressed("debug"):
+		update_classic();
+
+func _on_Coin_body_entered_trigger(body): #another custom node made specifically for this object
+	
+	queue_free() #and 'destroys' the object when colliding
+	
+	pass # Replace with function body.
