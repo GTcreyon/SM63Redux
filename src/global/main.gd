29277660c5a444344cf7585d8973e87cd6da231e
pokemeta <47202318+pokemeta@users.noreply.github.com>
2021-03-11 20:26:53 -0600

    W.I.P coin collect with hud system added
    Added a items_ready directory, made coin_item and coin script, and lastly changed gui to add the proper coin hud elements, though currently there's no sounds when the coin gets picked up, the changed scripts contains comments on how the new stuff works.

--- src/global/main.gd
+++ src/global/main.gd
@@ -1,3 +1,17 @@
 extends Node
 
 var classic = true;
+
+#the following lines are for the coins collected in the GUI
+var coins_total = 0
+
+onready var coin_counter_string = get_node("Player/Camera2D/GUI/Coin_counter") #note: this is to get ONLY the Coin_counter node from the GUI
+
+func _on_Coin_body_entered_collected(body): #custom node method made in this scene and it's connected to the main node
+	coins_total += 1
+	
+	coin_counter_string.text = str(coins_total) #str() converts the int variable to string, changing the number in Coin_counter
+	
+	pass # Replace with function body.
+
+#End of the lines for coin collect handle in this script
