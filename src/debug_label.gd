78d8bb39d59922e1db231c30b1860e207c06b79e
GTcreyon <plaidyoshi@gmail.com>
2021-03-14 18:02:15 +0000

    Added groundpound and cleared up some warnings

--- src/debug_label.gd
+++ src/debug_label.gd
@@ -1,6 +1,6 @@
 extends Label
 
-func _process(delta):
+func _process(_delta):
 	text = "Classic: " + ("On" if $"/root/Main".classic else "Off")
 	#text = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!?'\""
 	#text = String(get_parent().get_parent().position)
