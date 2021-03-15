074c30ae00ee8cc664091ee9f824b5ba4f520491
pokemeta <47202318+pokemeta@users.noreply.github.com>
2021-03-15 11:31:33 -0600

    added more coins, wrote comments and enemy AI has directional collision
    New GUI elements for red coins, variables, coin sfx, blue and red coins added, and the enemy AI counts with directional collision now.

--- src/global/main.gd
+++ src/global/main.gd
@@ -2,16 +2,48 @@ extends Node
 
 var classic = true
 
-#the following lines are for the coins collected in the GUI
+####the following lines are for the coins collected in the GUI
 var coins_total = 0
+var red_coins_total = 0 #specifically for red coins
 
-onready var coin_counter_string = get_node("Player/Camera2D/GUI/Coin_counter") #note: this is to get ONLY the Coin_counter node from the GUI
+onready var coin_counter_string = find_node("Coin_counter") #note: this is to get ONLY the Coin_counter node from the GUI
+onready var red_coin_counter_string = find_node("Red_coin_counter") #specifically for red coins
+onready var coin_audio_sfx = find_node("Coin_sfx")
 
-func _on_Coin_body_entered_collected(_body): #custom node method made in this scene and it's connected to the main node
-	coins_total += 1
-	
-	coin_counter_string.text = str(coins_total) #str() converts the int variable to string, changing the number in Coin_counter
+func _on_Coin_body_entered_collected(body): #custom node method made in this scene and it's connected to the main node
+	if body.is_in_group("mario"):
+		coin_audio_sfx.play()
+		coins_total += 1
+		
+		coin_counter_string.text = str(coins_total) #str() converts the int variable to string, changing the number in Coin_counter
 	
 	pass # Replace with function body.
 
-#End of the lines for coin collect handle in this script
+#the next function signals are for the non yellow coins
+
+func _on_Coin_red_body_entered_increment(body):
+	#this whole thing has extra stuff
+	#for the red coins
+	
+	if body.is_in_group("mario"):
+		coin_audio_sfx.play()
+		
+		red_coins_total += 1
+		coins_total += 2
+		
+		red_coin_counter_string.text = str(red_coins_total)
+		coin_counter_string.text = str(coins_total)
+	pass # Replace with function body.
+
+
+func _on_Coin_blue_body_entered_increment(body):
+	#same as yellow coins, but increments 5
+	#instead of 1
+	if body.is_in_group("mario"):
+		coin_audio_sfx.play()
+		coins_total += 5
+		
+		coin_counter_string.text = str(coins_total)
+	pass # Replace with function body.
+
+####End of the lines for coin collect handle in this script
