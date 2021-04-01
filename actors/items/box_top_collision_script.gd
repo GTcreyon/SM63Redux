f732d30523d2b0f947368244a7351f040662066e
pokemeta <47202318+pokemeta@users.noreply.github.com>
2021-04-01 10:53:31 -0600

    Other fludd and fludd boxes added
    Script handles their different types now, waiting for proper sprites to get them set and also fludd mechanic finished to complete these.

--- actors/items/box_top_collision_script.gd
+++ actors/items/box_top_collision_script.gd
@@ -1,15 +1,83 @@
-extends Area2D
+tool
+extends Node2D
+
+export(int, 3) var fludd_type = 0
 
 onready var main = $"/root/Main/Items"
+onready var fludd1 = $Box_Blue
+onready var fludd2 = $Box_Red
+onready var fludd3 = $Box_Gray
 
 var testloaderobj = preload("res://actors/items/fludd.tscn")
 var obj = testloaderobj.instance()
 
-func _on_Box_top_entered(body):
+func switch_type(fludd_type):
+	match fludd_type:
+		0:
+			fludd1.visible = false
+			fludd1.monitoring = false
+			fludd2.visible = false
+			fludd2.monitorable = false
+			fludd3.visible = false
+			fludd3.monitoring = false
+			
+		1:
+			fludd1.visible = true
+			fludd1.monitoring = true
+			fludd2.visible = false
+			fludd2.monitorable = false
+			fludd3.visible = false
+			fludd3.monitoring = false
+			
+		2:
+			fludd1.visible = false
+			fludd1.monitoring = false
+			fludd2.visible = true
+			fludd2.monitorable = true
+			fludd3.visible = false
+			fludd3.monitoring = false
+		
+		3:
+			fludd1.visible = false
+			fludd1.monitoring = false
+			fludd2.visible = false
+			fludd2.monitorable = false
+			fludd3.visible = true
+			fludd3.monitoring = true
+			
+func _process(_delta):
+	if Engine.editor_hint:
+		switch_type(fludd_type)
+
+func _ready():
+	switch_type(fludd_type)
+
+
+func _on_Box_Blue_body_entered(body):
 	if body.global_position.y < global_position.y && (body.global_position.x < global_position.x || body.global_position.x > global_position.x):
 		print("collided from top")
 		main.add_child(obj)
-		obj.position = get_parent().position
+		obj.position = position
+		
+		queue_free()
+	pass # Replace with function body.
+
+
+func _on_Box_Red_body_entered(body):
+	if body.global_position.y < global_position.y && (body.global_position.x < global_position.x || body.global_position.x > global_position.x):
+		print("collided from top")
+		main.add_child(obj)
+		obj.position = position
+		
+		queue_free()
+	pass # Replace with function body.
+
+
+func _on_Box_Gray_body_entered(body):
+	if body.global_position.y < global_position.y && (body.global_position.x < global_position.x || body.global_position.x > global_position.x):
+		print("collided from top")
+		main.add_child(obj)
+		obj.position = position
 		
 		queue_free()
 	pass # Replace with function body.
--- actors/items/fludd_gravity.gd
+++ actors/items/fludd_gravity.gd
@@ -5,6 +5,50 @@ const FLOOR = Vector2(0, -1)
 
 var velocity = Vector2()
 
+onready var main = $"/root/Main/Items/Fludd_box".type_fludd
+
+onready var fl1 = $Normal_fludd
+onready var fl2 = $Rocket_fludd
+onready var fl3 = $I_forgot_how_this_fludd_was_called
+
+func switch_type(main):
+	match main:
+		0:
+			fl1.visible = false
+			fl1.monitoring = false
+			fl2.visible = false
+			fl2.monitoring = false
+			fl3.visible = false
+			fl3.monitoring = false
+		1:
+			fl1.visible = true
+			fl1.monitoring = true
+			fl2.visible = false
+			fl2.monitoring = false
+			fl3.visible = false
+			fl3.monitoring = false
+		2:
+			fl1.visible = false
+			fl1.monitoring = false
+			fl2.visible = true
+			fl2.monitoring = true
+			fl3.visible = false
+			fl3.monitoring = false
+		3:
+			fl1.visible = false
+			fl1.monitoring = false
+			fl2.visible = false
+			fl2.monitoring = false
+			fl3.visible = true
+			fl3.monitoring = true
+
+func _process(_delta):
+	if Engine.editor_hint:
+		switch_type(main)
+
+func _ready():
+	switch_type(main)
+
 func _physics_process(_delta):
 	velocity.y += GRAVITY
 	
