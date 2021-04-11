f51265ae008c78318c699678c89dd29d5f15932a
jaschutte <34577095+jaschutte@users.noreply.github.com>
2021-04-11 18:37:36 +0200

    fixed importing issues + added demo of water in main

--- actors/water/water.gd
+++ actors/water/water.gd
@@ -1,11 +1,7 @@
 extends Area2D
 
-onready var texture = $Texture
-onready var collision = $Collision
-onready var mat = texture.material
-
 func _process(dt):
-	var pos = global_position #get the position
+	var pos = get_global_transform_with_canvas().origin #get the position
 	var size = OS.get_window_size() #normalise the object
 	pos.y = size.y - pos.y #inverse the y
-	mat.set_shader_param("object_pos", pos / size) #give the object position to the shader
+	material.set_shader_param("object_pos", pos / size) #give the object position to the shader
