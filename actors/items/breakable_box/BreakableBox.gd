8665a11d7941aea45813f03fef392d44f5ffca45
brickblock369 <59150779+brickblock369@users.noreply.github.com>
2021-04-11 11:23:59 -0700

    Add files via upload
    test

---
+++ actors/items/breakable_box/BreakableBox.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+onready var player = $"/root/Main/Player"
+
+
+
+
+func _on_DetectionArea_body_entered(body):
+	if (player.state == player.s.pound_fall && body.global_position.y < global_position.y) or (player.state == player.s.spin and (body.global_position.y > global_position.y || body.global_position.y < global_position.y) and player.spin_timer > 0): 
+		queue_free()
+
+
+#only make it breakable when it's spinning towards its sides, and when it's only the first few nanoseconds of the spinning.
