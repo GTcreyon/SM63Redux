3a48e9de298d2d341e291a34e15748196c728b9b
brickblock369 <59150779+brickblock369@users.noreply.github.com>
2021-04-27 22:14:51 -0700

    Koopa assets
    CODE IS INCOMPLETE AT THE MOMENT, testing purposes

---
+++ koopa/actors/KoopaCollision.gd
@@ -0,0 +1,13 @@
+extends "res://area2D_enemy_script.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
