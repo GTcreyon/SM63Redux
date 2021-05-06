e95707932a8b227164ac6a32887e4ccca1df1e6d
brickblock369 <59150779+brickblock369@users.noreply.github.com>
2021-05-05 23:02:14 -0700

    Adding door scene. Animation isn't done yet.

---
+++ actors/door/door.gd
@@ -0,0 +1,29 @@
+extends Area2D
+var enterable = 0
+
+onready var player = $"/root/Main/Player"
+export var target_x_pos = 0 
+export var target_y_pos = 0 
+
+
+func _physics_process(delta):
+	if enterable == 1 && Input.is_action_just_pressed("up"):
+		player.static_v = true
+		var t = Timer.new()
+		t.set_wait_time(1)
+		t.set_one_shot(true)
+		self.add_child(t)
+		t.start()
+		yield(t, "timeout")
+		t.queue_free()
+		player.position = Vector2(target_x_pos, target_y_pos) 
+		player.static_v = false
+
+func _on_Door_body_entered(body):
+	print("in")
+	enterable = 1
+
+
+func _on_Door_body_exited(body):
+	print("out")
+	enterable = 0
