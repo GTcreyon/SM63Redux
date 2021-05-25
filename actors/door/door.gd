116b2c831fa3ea7b013d813e8f50a13a7dac5cd2
brickblock369 <59150779+brickblock369@users.noreply.github.com>
2021-05-25 10:41:01 -0700

    Removed timer and used the += trick in _process() instead
    I suppose this works?

--- actors/door/door.gd
+++ actors/door/door.gd
@@ -6,33 +6,25 @@ export var target_x_pos = 0
 export var target_y_pos = 0 
 export var move_to_scene = false
 export var next_scene: PackedScene
-
+var timer = 0
+var entered = false
 func _physics_process(delta):
+	print(timer)
 	if enterable == 1 && Input.is_action_just_pressed("up"):
 		player.static_v = true
 		$DoorSprite.play("opening")
-		var t = Timer.new()
-		t.set_wait_time(1)
-		t.set_one_shot(true)
-		self.add_child(t)
-		t.start()
-		yield(t, "timeout")
-		$DoorSprite.play("closing")
-		t.set_wait_time(1)
-		t.set_one_shot(true)
-		t.start()
-		yield(t, "timeout")
-		t.queue_free()
+		entered = true
+	if entered == true:
+		timer += 1
+	if timer == 60:
 		if move_to_scene == true:
 			get_tree().change_scene_to(next_scene)
 		player.position = Vector2(target_x_pos, target_y_pos) 
 		player.static_v = false
-
+		entered = false
 func _on_Door_body_entered(body):
 	print("in")
 	enterable = 1
-
-
 func _on_Door_body_exited(body):
 	print("out")
 	enterable = 0
