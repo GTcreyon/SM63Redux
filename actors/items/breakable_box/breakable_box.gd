0b54f7a075060212bc35266a62f6c118286bfd94
GTcreyon <plaidyoshi@gmail.com>
2021-05-31 14:35:04 +0100

    Cleanup

--- actors/items/breakable_box/breakable_box.gd
+++ actors/items/breakable_box/breakable_box.gd
@@ -3,11 +3,8 @@ extends StaticBody2D
 onready var player = $"/root/Main/Player"
 
 
-var rng = RandomNumberGenerator.new()
 func _ready():
-	rng.randi()
-	var my_random_number = rng.randf_range(0, 2)
-	$Sprite.set_frame(my_random_number)
+	$Sprite.frame = randi() % 3
 
 func _on_DetectionArea_body_entered(body):
 	if (player.state == player.s.pound_fall && body.global_position.y < global_position.y) or (player.state == player.s.spin and (body.global_position.y > global_position.y || body.global_position.y < global_position.y) and player.spin_timer > 0): 
