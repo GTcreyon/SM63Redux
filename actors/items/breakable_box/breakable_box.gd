e6b88bbdfabb5ff5dc7639e6ed717eb465ec4146
GTcreyon <plaidyoshi@gmail.com>
2021-08-02 15:58:49 +0100

    Fixed cork boxes, HUD flash, etc.

--- actors/items/breakable_box/breakable_box.gd
+++ actors/items/breakable_box/breakable_box.gd
@@ -6,8 +6,18 @@ onready var player = $"/root/Main/Player"
 func _ready():
 	$Sprite.frame = randi() % 3
 
+
+func _process(_delta):
+	if $PoundArea.overlaps_body(player):
+		if player.state == player.s.pound_fall:
+			queue_free()
+	if $SpinArea.overlaps_body(player):
+		if player.state == player.s.spin && player.spin_timer > 0: 
+			queue_free()
+
 func _on_DetectionArea_body_entered(body):
-	if (player.state == player.s.pound_fall && body.global_position.y < global_position.y) or (player.state == player.s.spin and (body.global_position.y > global_position.y || body.global_position.y < global_position.y) and player.spin_timer > 0): 
+	if ((player.state == player.s.pound_fall && body.global_position.y < global_position.y)
+	|| (player.state == player.s.spin && player.spin_timer > 0)): 
 		queue_free()
 
 
