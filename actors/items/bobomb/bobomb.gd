e3e0898e860512a1f27d1e1e065c7e215f359657
GTcreyon <plaidyoshi@gmail.com>
2021-08-03 17:38:48 +0100

    Reworked goomba, can now be spun

---
+++ actors/items/bobomb/bobomb.gd
@@ -0,0 +1,2 @@
+extends KinematicBody2D
+
--- actors/items/goomba/goomba.gd
+++ actors/items/goomba/goomba.gd
@@ -2,13 +2,14 @@ extends KinematicBody2D
 
 const GRAVITY = 10
 const FLOOR = Vector2(0, -1)
+const coin = preload("res://actors/items/coin/coin_yellow.tscn")
 const sfx = {
 	"jump": preload("res://audio/sfx/items/goomba/goomba_jump.ogg"),
 	"step": preload("res://audio/sfx/items/goomba/goomba_step.wav"),
 	#"squish": preload("res://audio/sfx/items/goomba/goomba_jump.ogg"),
 	}
 
-var velocity = Vector2()
+var vel = Vector2()
 
 export var direction = 1
 
@@ -18,13 +19,18 @@ var is_jumping = false #this is for stopping goomba's movement and then
 var tracking = false
 var wander_dist = 0
 var stepped = false
+var full_jump = false
+var dead = false
+var struck = false
 
 onready var sprite = $Sprite
 onready var raycast = $RayCast2D
 onready var player = $"/root/Main/Player"
 onready var sfx_active = $SFXActive
 onready var sfx_passive = $SFXPassive
-
+onready var main = $"/root/Main"
+onready var lm_counter = player.life_meter_counter
+onready var lm_gui = $"/root/Main/Player/Camera2D/GUI/LifeMeterCounter"
 var land_timer = 0
 
 func _ready():
@@ -32,13 +38,47 @@ func _ready():
 
 
 func _physics_process(_delta):
+	if sprite.animation == "squish":
+		if dead:
+			var spawn = coin.instance()
+			spawn.position = position
+			spawn.dropped = true
+			main.add_child(spawn)
+			queue_free()
+		elif !struck:
+			if player.position.y + 16 > global_position.y - 10:
+				player.vel.y = 0
+				player.position.y += 0.5
+			if Input.is_action_just_pressed("jump"):
+				full_jump = true
+			
+		if sprite.frame == 3:
+			if !struck:
+				if Input.is_action_pressed("jump"):
+					if full_jump:
+						player.vel.y = -6.5
+					else:
+						player.vel.y = -6
+				else:
+					player.vel.y = -5
+				player.switch_state(player.s.walk)
+			dead = true #apparently queue_free() doesn't cancel the current cycle
+			
+	#code to push enemies apart - maybe come back to later?
+#	for area in get_overlapping_areas():
+#		if area != player:
+#			if global_position.x > area.global_position.x || (global_position.x == area.global_position.x && id > area.id):
+#				get_parent().vel.x += 7.5
+#			else:
+#				get_parent().vel.x -= 7.5
+
 	if !is_on_floor() && sprite.animation != "squish":
 		sprite.frame = 1
 		raycast.enabled = false
 	
-	velocity.y += GRAVITY
+	vel.y += GRAVITY
 	
-	if sprite.animation != "squish":
+	if sprite.animation != "squish" && !struck:
 		#raycast2d is used here to detect if the object collided with a wall
 		#to change directions
 		if direction == 1:
@@ -62,7 +102,6 @@ func _physics_process(_delta):
 					raycast.enabled = true
 				
 				land_timer += 0.2
-				print(str(sprite.frame) + " " + str(land_timer))
 				if land_timer >= 1.8:
 					sprite.frame = 0
 					sprite.animation = "walking"
@@ -77,28 +116,28 @@ func _physics_process(_delta):
 				else:
 					stepped = false
 				if tracking:
-					sprite.speed_scale = abs(velocity.x) / 100 + 1
+					sprite.speed_scale = abs(vel.x) / 100 + 1
 					if player.position.x - position.x < -20 || (player.position.x < position.x && abs(player.position.y - position.y) < 26):
-						velocity.x = max(velocity.x - 5, -100)
+						vel.x = max(vel.x - 5, -100)
 						direction = -1
 						raycast.position.x = -9
 						sprite.playing = true
 					elif player.position.x - position.x > 20 || (player.position.x > position.x && abs(player.position.y - position.y) < 26):
-						velocity.x = min(velocity.x + 5, 100)
+						vel.x = min(vel.x + 5, 100)
 						direction = 1
 						raycast.position.x = 9
 						sprite.playing = true
 					else:
-						velocity.x *= 0.85
+						vel.x *= 0.85
 						if sprite.frame == 0:
 							sprite.playing = false
 				else:
 					sprite.speed_scale = 1
 					sprite.playing = true
 					if direction == 1:
-						velocity.x = min(velocity.x + 5, 50)
+						vel.x = min(vel.x + 5, 50)
 					else:
-						velocity.x = max(velocity.x - 5, -50)
+						vel.x = max(vel.x - 5, -50)
 					wander_dist += 1
 					if wander_dist >= 120 && sprite.frame == 0:
 						wander_dist = 0
@@ -108,9 +147,14 @@ func _physics_process(_delta):
 			if !is_jumping:
 				sprite.frame = 1
 		
-	velocity = move_and_slide(velocity, FLOOR)
+	vel = move_and_slide(vel, FLOOR)
+	if is_on_floor() && struck && sprite.animation != "squish":
+		sprite.animation = "squish"
+		sprite.frame = 0
+		sprite.playing = true
+		
 #the next signals are used for the aggresive trigger
-#behaviour, it changes the velocity and goes towards
+#behaviour, it changes the vel and goes towards
 #the player, it also changes the raycast2d because
 #after mario goes away, the enemy returns to its
 #pacific state
@@ -132,7 +176,7 @@ func _on_Collision_mario_detected(body):
 			sfx_active.play()
 			sprite.frame = 0
 			is_jumping = true
-			velocity.y = -150
+			vel.y = -150
 		wander_dist = 0
 
 
@@ -143,5 +187,29 @@ func flip_ev():
 
 func _on_AwareArea_body_exited(body):
 	if body == player:
-		print("lost")
 		tracking = false
+
+
+func _on_Area2D_body_entered_hurt(body):
+	if body == player:
+		if sprite.animation != "squish":
+			if body.hitbox.global_position.y + body.hitbox.shape.extents.y - body.vel.y - 6 < position.y && body.vel.y > 0:
+				sprite.animation = "squish"
+				struck = false
+				vel.y = 0
+				sprite.frame = 0
+				sprite.playing = true
+				player.switch_state(player.s.ejump)
+			elif !struck:
+				if player.state == player.s.spin && player.spin_timer > 0:
+					struck = true
+					vel.y -= 158
+					sprite.animation = "jumping"
+					vel.x += max((12 + vel.x * sign(player.position.x - position.x) / 1.5), 0) * 5.4 
+				else:
+					lm_counter -= 1
+					lm_gui.text = str(lm_counter)
+					
+					player.vel.x += 4 * sign(body.position.x - position.x)
+					player.vel.y += -8
+
