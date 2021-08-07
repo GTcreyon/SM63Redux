6ac74f23f351250e953a274be2bb1f0d0d5a67c4
GTcreyon <plaidyoshi@gmail.com>
2021-08-07 21:40:00 +0100

    bubble pain

--- actors/player/bubbles.gd
+++ actors/player/bubbles.gd
@@ -9,3 +9,17 @@ func _ready():
 	texture = tex
 	#now give the shader our viewport texture
 	material.set_shader_param("viewport_texture", viewport.get_texture())
+	
+	#deparent this node
+	$"/root/Main/Player".call_deferred("remove_child", self)
+	$"/root/Main".call_deferred("add_child", self)
+	#deparent the viewport
+	$"/root/Main/Player".call_deferred("remove_child", viewport)
+	$"/root/Main".call_deferred("add_child", viewport)
+
+
+func _process(delta):
+	position = get_viewport_rect().size / 2 - get_viewport().get_canvas_transform().origin
+	var o = $"/root/Main/Player".get_global_transform_with_canvas().origin
+	$"/root/Main/BubbleViewport/BubblesSmall".position = o
+	$"/root/Main/BubbleViewport/BubblesMedium".position = o
