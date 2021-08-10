efaea558a957b4aa61cefc4e06fcb64698a8020f
GTcreyon <plaidyoshi@gmail.com>
2021-08-10 22:45:39 +0100

    Odds and ends

--- actors/items/koopa/koopa_shell.gd
+++ actors/items/koopa/koopa_shell.gd
@@ -2,39 +2,33 @@ extends KinematicBody2D
 onready var lm_counter = $"/root/Main/Player".life_meter_counter
 onready var lm_gui = $"/root/Main/Player/Camera2D/GUI/Life_meter_counter"
 onready var player = $"/root/Main/Player/AnimatedSprite"
-const GRAVITY = 10
-var speed = 200
-const FLOOR = Vector2(0, -1)
-
-var velocity = Vector2()
+var speed = 5
+var vel = Vector2.ZERO
 
 export var direction = 1
 
-	
-func _ready():
-	velocity.x = 0
-	
 func _physics_process(_delta):
-	velocity.y += GRAVITY
-	velocity = move_and_slide(velocity, FLOOR)
-	#print(direction)
-	if is_on_wall():
+	vel.y = min(vel.y + 0.3, 6)
+	move_and_slide(vel * 60, Vector2.UP)
+	vel.x = lerp(vel.x, 0, 0.00625)
+	if is_on_floor():
+		vel.y = 0
+	if is_on_wall(): #flip when hitting wall
 		direction *= -1
-	
+		vel.x *= -1
+
+
 func _on_CollisionArea_body_entered(body):
 	if body.hitbox.global_position.y + body.hitbox.shape.extents.y < global_position.y && body.vel.y > 0:
 		print("collided from top")
 		$"/root/Main/Player".vel.y = -5
 		if body.global_position.x < global_position.x:
-			velocity.x = speed
+			vel.x = speed
 		else:
-			velocity.x = -speed
+			vel.x = -speed
 	elif body.global_position.x < global_position.x:
 		print("collided from left")
-		#if it's less than, then x-wise it's left and y-wise it's up.
-		velocity.x = speed
+		vel.x = speed
 	elif body.global_position.x > global_position.x:
 		print("collided from right")
-		velocity.x = -speed
-
-		
+		vel.x = -speed
