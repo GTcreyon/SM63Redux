cbb9efa12bfb2d63c76e7ae0295689b79483b750
GTcreyon <plaidyoshi@gmail.com>
2021-08-22 19:44:41 +0100

    More dialog stuff

---
+++ hud/buttons/input_display.gd
@@ -0,0 +1,15 @@
+extends AnimatedSprite
+
+export(String) var input
+
+var intensity = 0
+
+func _process(delta):
+	intensity = max(intensity - 0.05, 0)
+	if Input.is_action_pressed(input):
+		animation = "on"
+		if Input.is_action_just_pressed(input):
+			intensity = 1
+	else:
+		animation = "off"
+	material.set_shader_param("outline_color", Color(1, 1, 1, intensity))
