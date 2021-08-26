141ff2d72b4280be63feddcf1f22032212e59ac9
GTcreyon <plaidyoshi@gmail.com>
2021-08-26 12:19:24 +0100

    Coyote time and quelling some warnings

--- actors/items/coin/coin_blue.gd
+++ actors/items/coin/coin_blue.gd
@@ -1,6 +1,6 @@
 extends "res://actors/items/coin/coin.gd"
 
-func _on_PickupArea_body_entered(body):
+func _on_PickupArea_body_entered(_body):
 	singleton.coin_total += 5
 	$SFX.play()
 	$PickupArea.queue_free() #clears up the acting segments of the coin so only the SFX is left
