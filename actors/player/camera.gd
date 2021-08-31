a4d7f329f437c4271381bd6b634fc02e2f463e64
GTcreyon <plaidyoshi@gmail.com>
2021-09-01 00:04:31 +0100

    Pause

--- actors/player/camera.gd
+++ actors/player/camera.gd
@@ -5,37 +5,16 @@ var current_zoom = 1.0
 var target_zoom = 1.0
 
 func _process(_delta):
-	var size_changed = false
-	if Input.is_action_just_pressed("fullscreen"):
-		OS.window_fullscreen = !OS.window_fullscreen
-		size_changed = true
-	if Input.is_action_just_pressed("screen+") && OS.window_size.x + 448 <= OS.get_screen_size().x && OS.window_size.y + 304 <= OS.get_screen_size().y:
-		OS.window_size.x += 448
-		OS.window_size.y += 304
-		size_changed = true
-	if Input.is_action_just_pressed("screen-") && OS.window_size.x - 448 >= 448:
-		OS.window_size.x -= 448
-		OS.window_size.y -= 304
-		size_changed = true
-	
-	#$GUI.set_size(log(floor(OS.window_size.x / 448)) / log(2) + 1)
-	#$GUI.set_size(floor(OS.window_size.x / 448))
-	
 	var zoom_factor = 448/OS.window_size.x
-	
-	if Input.is_action_just_pressed("zoom+"):
-		target_zoom /= 2
-		tween.stop_all()
-		tween.interpolate_property(self, "current_zoom", null, target_zoom, 0.5, tween.TRANS_EXPO, Tween.EASE_OUT, 0)
-		tween.start()
-	if Input.is_action_just_pressed("zoom-"):
-		target_zoom *= 2
-		tween.stop_all()
-		tween.interpolate_property(self, "current_zoom", null, target_zoom, 0.5, tween.TRANS_EXPO, Tween.EASE_OUT, 0)
-		tween.start()
-		
-	if size_changed:
-		$"/root/Main/Bubbles".refresh()
-		$GUI.set_size(floor(log(floor(OS.window_size.x / 448)) / log(2) + 1), floor(OS.window_size.x / 448))
-	
+	if !get_tree().paused:
+		if Input.is_action_just_pressed("zoom+"):
+			target_zoom /= 2
+			tween.stop_all()
+			tween.interpolate_property(self, "current_zoom", null, target_zoom, 0.5, tween.TRANS_EXPO, Tween.EASE_OUT, 0)
+			tween.start()
+		if Input.is_action_just_pressed("zoom-"):
+			target_zoom *= 2
+			tween.stop_all()
+			tween.interpolate_property(self, "current_zoom", null, target_zoom, 0.5, tween.TRANS_EXPO, Tween.EASE_OUT, 0)
+			tween.start()
 	zoom = Vector2.ONE * zoom_factor * current_zoom
--- hud/gui.gd
+++ hud/gui.gd
@@ -10,6 +10,45 @@ onready var meter = $MeterControl
 onready var icon = $MeterControl/Icon
 
 var icon_bob = 0
+var pause_offset = 0
+func _ready():
+	resize()
+
+
+func resize():
+	var scale = floor(OS.window_size.y / 304)
+	var topsize = OS.window_size.x / scale - 36 - 30
+	var offset = 38 / 2 - floor((int(topsize) % 38) / 2.0)
+	$Top.rect_scale = Vector2.ONE * scale
+	$Top.rect_size.x = topsize + offset + 19 * scale
+	$Top.rect_position.x = 29 * scale - offset * scale - 19 * scale
+	$LeftCornerTop.rect_scale = Vector2.ONE * scale
+	$LeftCornerBottom.rect_scale = Vector2.ONE * scale
+	$RightCornerTop.rect_scale = Vector2.ONE * scale
+	$RightCornerBottom.rect_scale = Vector2.ONE * scale
+	$Left.rect_scale = Vector2.ONE * scale
+	$Left.rect_position.y = 17 * scale
+	$Left.rect_size.y = OS.window_size.y / scale - 17 - 33
+	$Right.rect_scale = Vector2.ONE * scale
+	$Right.rect_position.y = 17 * scale
+	$Right.rect_size.y = OS.window_size.y / scale - 17 - 33
+	
+	$ButtonMap.rect_scale = Vector2.ONE * scale
+	$ButtonMap.rect_position.x = 29 * scale
+	$ButtonMap.rect_size.x = floor((OS.window_size.x - 61 * scale) / scale / 4)
+	
+	$ButtonFludd.rect_scale = Vector2.ONE * scale
+	$ButtonFludd.rect_position.x = $ButtonMap.rect_position.x + $ButtonMap.rect_size.x * scale - 1 * scale
+	$ButtonFludd.rect_size.x = ceil((OS.window_size.x - 61 * scale) / scale / 4)
+	
+	$ButtonOptions.rect_scale = Vector2.ONE * scale
+	$ButtonOptions.rect_position.x = $ButtonFludd.rect_position.x + $ButtonFludd.rect_size.x * scale - 1 * scale
+	$ButtonOptions.rect_size.x = floor((OS.window_size.x - 61 * scale) / scale / 4)
+	
+	$ButtonExit.rect_scale = Vector2.ONE * scale
+	$ButtonExit.rect_position.x = $ButtonOptions.rect_position.x + $ButtonOptions.rect_size.x * scale - 1 * scale
+	$ButtonExit.rect_size.x = floor((OS.window_size.x - 61 * scale) / scale / 4)
+
 
 func set_size(size, lin_size):
 	#size: general size of UI elements
@@ -23,6 +62,7 @@ func set_size(size, lin_size):
 	$DialogBox.gui_size = lin_size
 	$InputDisplay.rect_scale = Vector2.ONE * size
 	$InputDisplay.rect_position = Vector2(2 * size, 71 * size)
+	resize()
 
 
 func _process(_delta):
@@ -52,3 +92,40 @@ func _process(_delta):
 		icon_bob = fmod(icon_bob + 0.1, 120)
 	icon.offset.y = sin(icon_bob) * 2
 	
+	var size_changed = false
+	if Input.is_action_just_pressed("fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+		size_changed = true
+	if Input.is_action_just_pressed("screen+") && OS.window_size.x + 448 <= OS.get_screen_size().x && OS.window_size.y + 304 <= OS.get_screen_size().y:
+		OS.window_size.x += 448
+		OS.window_size.y += 304
+		size_changed = true
+	if Input.is_action_just_pressed("screen-") && OS.window_size.x - 448 >= 448:
+		OS.window_size.x -= 448
+		OS.window_size.y -= 304
+		size_changed = true
+	if size_changed:
+		$"/root/Main/Bubbles".refresh()
+		set_size(floor(log(floor(OS.window_size.x / 448)) / log(2) + 1), floor(OS.window_size.x / 448))
+	
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = !get_tree().paused
+	
+	var menu = get_tree().get_nodes_in_group("pause")
+	var gui_scale = floor(OS.window_size.y / 304)
+	if get_tree().paused:
+		pause_offset = lerp(pause_offset, 1, 0.5)
+		for node in menu:
+			node.modulate.a = min(node.modulate.a + 0.2, 1)
+	else:
+		pause_offset = lerp(pause_offset, 0, 0.5)
+		for node in menu:
+			node.modulate.a = max(node.modulate.a - 0.2, 0)
+	$StatsTL.margin_left = 8 + (37 * gui_scale) * pause_offset
+	$StatsTL.margin_top = 8 + (19 * gui_scale) * pause_offset
+	$StatsTR.margin_left = -8 - (37 * gui_scale) * pause_offset
+	$StatsTR.margin_top = 8 + (19 * gui_scale) * pause_offset
+	$StatsBL.margin_left = 8 + (37 * gui_scale) * pause_offset
+	$StatsBL.margin_top = -8 - (33 * gui_scale) * pause_offset
+	$MeterControl.margin_left = -57 - (37 * gui_scale) * pause_offset
+	$MeterControl.margin_top = -113 - (33 * gui_scale) * pause_offset
