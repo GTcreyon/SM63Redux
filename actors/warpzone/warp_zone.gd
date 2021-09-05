b6767160137bf968556bb58f55c6d36f17edf917
GTcreyon <plaidyoshi@gmail.com>
2021-09-05 19:26:02 +0100

    Warp zone baseline

--- actors/warpzone/warp_zone.gd
+++ actors/warpzone/warp_zone.gd
@@ -1,23 +1,16 @@
 extends Area2D
 
-onready var player = $"/root/Main/Player"
 onready var sweep_effect = $"/root/Main/Player/Camera2D/GUI/SweepEffect" 
-export var set_location = Vector2()
-var turn_on = false
-func _ready():
-	sweep_effect.rect_position.x = -800
-	sweep_effect.rect_position.y = 0
-	
-func _physics_process(delta):
-	if turn_on == true:
-		sweep_effect.visible = true
-		sweep_effect.rect_position.x += 20
-	if sweep_effect.rect_position.x == -200 and turn_on == true:
-		player.position = set_location
-	if sweep_effect.rect_position.x == 1500:
-		sweep_effect.set_visible(false)
-		turn_on = false
-func _on_WarpZone_body_entered(body):
-	sweep_effect.set_visible(true)
-	turn_on = true
-	sweep_effect.rect_position.x = -800
+export var set_location : Vector2
+export var scene_path : String
+
+#func _ready():
+#	sweep_effect.rect_position.x = -800
+#	sweep_effect.rect_position.y = 0
+
+
+func _on_WarpZone_body_entered(_body):
+	sweep_effect.visible = true
+	sweep_effect.turn_on = true
+	sweep_effect.set_location = set_location
+	sweep_effect.scene_path = scene_path
