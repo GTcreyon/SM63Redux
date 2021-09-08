d509a4cf11e90612ebfe86b9a20da4f67bd63863
GTcreyon <plaidyoshi@gmail.com>
2021-09-08 18:05:55 +0100

    Camera limits

--- actors/camera_area.gd
+++ actors/camera_area.gd
@@ -10,13 +10,18 @@ func set_size(new_size):
 	size = new_size
 
 
+
+
+
 func _on_Area2D_body_entered(_body):
 	if !Engine.editor_hint:
 		if limits & 1:
-			$"/root/Main/Player/Camera2D".limit_right = position.x + $CollisionShape2D.shape.extents.x
+			$"/root/Main/Player/Camera2D".target_limit_left = position.x - $CollisionShape2D.shape.extents.x
+			print($"/root/Main/Player/Camera2D".target_limit_left)
 		if limits & 2:
-			$"/root/Main/Player/Camera2D".limit_left = position.x - $CollisionShape2D.shape.extents.x
+			$"/root/Main/Player/Camera2D".target_limit_right = position.x + $CollisionShape2D.shape.extents.x
+			print($"/root/Main/Player/Camera2D".target_limit_right)
 		if limits & 4:
-			$"/root/Main/Player/Camera2D".limit_bottom = position.y + $CollisionShape2D.shape.extents.y
+			$"/root/Main/Player/Camera2D".target_limit_top = position.y - $CollisionShape2D.shape.extents.y
 		if limits & 8:
-			$"/root/Main/Player/Camera2D".limit_top = position.y - $CollisionShape2D.shape.extents.y
+			$"/root/Main/Player/Camera2D".target_limit_bottom = position.y + $CollisionShape2D.shape.extents.y
