e5ac8be506d0ee7cf60d3f144da55356ba612459
GTcreyon <plaidyoshi@gmail.com>
2021-09-08 18:05:55 +0100

    Camera limits

--- actors/player/camera.gd
+++ actors/player/camera.gd
@@ -3,6 +3,11 @@ extends Camera2D
 onready var tween = $Tween
 var current_zoom = 1.0
 var target_zoom = 1.0
+var target_limit_left = -10000000
+var target_limit_right = 10000000
+var target_limit_top = -10000000
+var target_limit_bottom = 10000000
+var first_frame = true
 
 func _ready():
 	limit_left = -10000000
@@ -25,3 +30,14 @@ func _process(_delta):
 			tween.interpolate_property(self, "current_zoom", null, target_zoom, 0.5, tween.TRANS_EXPO, Tween.EASE_OUT, 0)
 			tween.start()
 	zoom = Vector2.ONE * zoom_factor * current_zoom
+	if first_frame:
+		limit_left = target_limit_left
+		limit_right = target_limit_right
+		limit_top = target_limit_top
+		limit_bottom = target_limit_bottom
+		first_frame = false
+	else:
+		limit_left = lerp(limit_left, target_limit_left, 0.05)
+		limit_right = lerp(limit_right, target_limit_right, 0.05)
+		limit_top = lerp(limit_top, target_limit_top, 0.05)
+		limit_bottom = lerp(limit_bottom, target_limit_bottom, 0.05)
