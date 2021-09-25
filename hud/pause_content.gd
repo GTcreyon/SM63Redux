7f5b1d4f0da42e1419912cfea637fa052d24b618
GTcreyon <plaidyoshi@gmail.com>
2021-09-25 22:38:50 +0100

    Pause menu switching

--- hud/pause_content.gd
+++ hud/pause_content.gd
@@ -5,9 +5,32 @@ onready var map = $MapMenu
 onready var fludd = $FluddMenu
 onready var options = $OptionsMenu
 onready var exit = $ExitMenu
+onready var stats = get_tree().get_nodes_in_group("stats")
+onready var map_button = get_parent().get_node("ButtonMap")
+onready var fludd_button = get_parent().get_node("ButtonFludd")
+onready var options_button = get_parent().get_node("ButtonOptions")
+onready var exit_button = get_parent().get_node("ButtonExit")
 
-#func _process(delta):
-#	print(rect_size)
+func _process(_delta):
+	map.visible = false
+	fludd.visible = false
+	options.visible = false
+	exit.visible = false
+	info.visible = false
+	for node in stats:
+		node.visible = false
+	if map_button.pressed:
+		map.visible = true
+	elif fludd_button.pressed:
+		fludd.visible = true
+	elif options_button.pressed:
+		options.visible = true
+	elif exit_button.pressed:
+		exit.visible = true
+	else:
+		info.visible = true
+		for node in stats:
+			node.visible = true
 
 func resize(scale):
 	rect_scale = Vector2.ONE * scale
