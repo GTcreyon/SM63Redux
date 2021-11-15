fe86237478fe557f4d83bded6e02598fd5c90009
GTcreyon <plaidyoshi@gmail.com>
2021-11-15 21:22:57 +0000

    Fixed parallax

--- actors/bg/bg.gd
+++ actors/bg/bg.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 func _process(_delta):
+	var cam_pos = $"/root/Main/Player/Camera2D".get_camera_position()
 	if OS.window_size.x > 448:
 		$Sky.offset = Vector2.ZERO
 		$Sky.scale = Vector2.ONE / 2
@@ -10,10 +11,10 @@ func _process(_delta):
 	
 	#$Layer2.texture_offset.x = -get_viewport().canvas_transform.get_origin().x / 5
 	#$Sky.position.y = - $"/root/Main/Player".position.y / 20
-	$Layer1.texture_offset.x = $"/root/Main/Player".position.x / 10
-	$Layer1.position.y = max(211, 211 - $"/root/Main/Player".position.y / 20)
-	$Layer2.texture_offset.x = $"/root/Main/Player".position.x / 20
-	$Layer2.position.y = -70 -$"/root/Main/Player".position.y / 50
-	$Layer3.texture_offset.x = $"/root/Main/Player".position.x / 10
-	$Layer3.position.y = lerp($Layer3.position.y, max(76, 76 -$"/root/Main/Player".position.y / 5), 0.05)
+	$Layer1.texture_offset.x = cam_pos.x / 10
+	$Layer1.position.y = max(211, 211 - cam_pos.y / 20)
+	$Layer2.texture_offset.x = cam_pos.x / 20
+	$Layer2.position.y = -70 -cam_pos.y / 50
+	$Layer3.texture_offset.x = cam_pos.x / 10
+	$Layer3.position.y = lerp($Layer3.position.y, max(76, 76 -cam_pos.y / 5), 0.05)
 	scale = Vector2.ONE * OS.window_size.x / 448
