b34dc5185dff234fa9d212f758a21b45c289b989
GTcreyon <plaidyoshi@gmail.com>
2021-11-18 20:23:34 +0000

    Bird looping

--- actors/items/goonie/goonie.gd
+++ actors/items/goonie/goonie.gd
@@ -1,11 +1,16 @@
 extends StaticBody2D
 
+
 var rng = RandomNumberGenerator.new()
 var time_count = 0
 var vel = Vector2.ZERO
 var riding = 0
 var block_riders = []
+var reset_position = position
+var reset_timer = 90
+var camera_polygon
 
+onready var camera_area = $"/root/Main/CameraArea"
 onready var sprite = $AnimatedSprite
 onready var ride_area = $RideArea
 
@@ -13,6 +18,15 @@ const up_time = 250.0*60/32 #maximum time a bird will flap up
 const down_time = 45.0*60/32 #maximum time a bird will swoop down
 
 func _ready():
+	camera_polygon = Geometry.offset_polygon_2d(camera_area.polygon, 224)[0]
+	var points = camera_polygon.size()
+	for i in range(points):
+		var from : Vector2 = camera_polygon[i]
+		var to : Vector2 = camera_polygon[(i + 1) % points]
+		var intersect = Geometry.segment_intersects_segment_2d(from, to, position, Vector2(-99999, 0))
+		if intersect != null:
+			print(intersect)
+			reset_position = intersect + Vector2.LEFT * 20
 	rng.seed = hash(position.x + position.y * PI) #each bird will be different, but behave the same way each time
 	sprite.frame = rng.seed % 7
 	sprite.playing = true
@@ -56,6 +70,16 @@ func _physics_process(_delta):
 	
 	if riding <= 0:
 		time_count += 1
+	
+	if camera_area != null:
+		if Geometry.is_point_in_polygon(position, camera_polygon):
+			reset_timer = 90
+		else:
+			reset_timer -= 1
+			if reset_timer <= 0:
+				position = reset_position
+				print("reset" + str(reset_position))
+				reset_timer = 90
 
 
 func _on_RideArea_body_entered(body):
