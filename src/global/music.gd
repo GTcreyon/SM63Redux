a0eaa9637f494611fd270a5a64d629d7afe87102
GTcreyon <plaidyoshi@gmail.com>
2021-11-22 21:56:32 +0000

    Main menu + some gui filesystem reorganisation

--- src/global/music.gd
+++ src/global/music.gd
@@ -1,4 +1,10 @@
 extends AudioStreamPlayer
 
 func _ready():
-	play()
+	if get_tree().get_current_scene().get_filename().count("tutorial"):
+		play()
+
+
+func _process(_delta):
+	if !get_tree().get_current_scene().get_filename().count("tutorial"):
+		stop()
