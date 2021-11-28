0ec52febb95709eec8566e31034bef7ac8050ed6
GTcreyon <plaidyoshi@gmail.com>
2021-11-28 11:51:00 +0000

    Fludd console cheat

--- actors/debug/console.gd
+++ actors/debug/console.gd
@@ -96,6 +96,18 @@ func run_command(cmd):
 		"menu":
 			#warning-ignore:RETURN_VALUE_DISCARDED
 			get_tree().change_scene("res://scenes/start/menu.tscn")
+		"fludd":
+			match args[1]:
+				"h", "hover", "0":
+					Singleton.collected_nozzles[0] = !Singleton.collected_nozzles[0]
+				"r", "rocket", "1":
+					Singleton.collected_nozzles[1] = !Singleton.collected_nozzles[1]
+				"t", "turbo", "2":
+					Singleton.collected_nozzles[2] = !Singleton.collected_nozzles[2]
+				"all":
+					Singleton.collected_nozzles = [true, true, true]
+				"none":
+					Singleton.collected_nozzles = [false, false, false]
 		_:
 			output("Unknown command \"%s\"." % args[0])
 
