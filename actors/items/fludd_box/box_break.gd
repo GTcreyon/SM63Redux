7fc9c316fb14b382b8c31238269bce2b67c6cce3
GTcreyon <plaidyoshi@gmail.com>
2021-12-06 21:41:43 +0000

    Fludd box breaking animation

---
+++ actors/items/fludd_box/box_break.gd
@@ -0,0 +1,20 @@
+extends AnimatedSprite
+
+var first_frame = false
+
+func _ready():
+	frame = 0 #needed to prevent desync ickyness
+
+
+func _process(_delta):
+	if animation.begins_with("bounce_"):
+		if first_frame && frame == 0:
+			first_frame = false
+			animation = "open_" + animation.substr(7)
+		if frame > 0:
+			first_frame = true
+	else:
+		if first_frame && frame == 0:
+			queue_free()
+		if frame > 0:
+			first_frame = true
