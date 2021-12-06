344b817b8c2ef34cd4724176a33e5a5d5ac4e510
GTcreyon <plaidyoshi@gmail.com>
2021-12-06 21:41:43 +0000

    Fludd box breaking animation

--- actors/items/fludd_box/fludd_box.gd
+++ actors/items/fludd_box/fludd_box.gd
@@ -10,7 +10,8 @@ onready var hover = $Hover
 onready var rocket = $Rocket
 onready var turbo = $Turbo
 
-var obj = preload("res://actors/items/fludd_box/fludd.tscn").instance()
+var fludd = preload("res://actors/items/fludd_box/fludd.tscn").instance()
+var break_anim = preload("res://actors/items/fludd_box/box_break.tscn").instance()
 
 func set_type(new_type):
 	type = new_type
@@ -31,9 +32,18 @@ func set_type(new_type):
 
 func _on_FluddBox_body_entered(body):
 	if body == player && player.vel.y > 1:
-		main.call_deferred("add_child", obj)
-		obj.position = Vector2(position.x, position.y + 8.5)
-		obj.call_deferred("switch_type", type)
+		main.call_deferred("add_child", break_anim)
+		break_anim.position = Vector2(position.x, position.y)
+		match type:
+			2:
+				break_anim.animation = "bounce_turbo"
+			1:
+				break_anim.animation = "bounce_rocket"
+			_:
+				break_anim.animation = "bounce_hover"
+		main.call_deferred("add_child", fludd)
+		fludd.position = Vector2(position.x, position.y + 8.5)
+		fludd.call_deferred("switch_type", type)
 		singleton.collected_nozzles[type] = true
 		player.vel.y = -6 * 32 / 60
 		$Open.play()
