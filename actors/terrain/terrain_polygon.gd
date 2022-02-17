bae53368183872d23d203b562e3293ea27f80163
jaschutte <34577095+jaschutte@users.noreply.github.com>
2022-02-17 21:08:02 +0100

    bottom texture terrain

--- actors/terrain/terrain_polygon.gd
+++ actors/terrain/terrain_polygon.gd
@@ -5,11 +5,21 @@ export(Texture) var body
 export(Texture) var top
 export(Texture) var top_left
 export(Texture) var top_right
+export(Texture) var edge
+export(Texture) var bottom
 
-export(Vector2) var up_direction = Vector2(0, -1)
+export(Vector2) var up_direction = Vector2(0, -1) setget set_down_direction
+export(Vector2) var down_direction = Vector2(0, 1) setget set_null
 export(int) var max_deviation = 60
 
 onready var decorations = $Decorations
+
+func set_down_direction(new_val):
+	up_direction = new_val
+	down_direction = new_val.tangent().tangent()
+
+func set_null(_new_val):
+	pass
 
 func _draw():
 	decorations.update()
