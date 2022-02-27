a518e5436d81ab1e98eb68c1940e0a73c526d7ed
GTcreyon <plaidyoshi@gmail.com>
2022-02-27 15:59:31 +0000

    volume controls + sign sfx

--- actors/items/sign/sign.gd
+++ actors/items/sign/sign.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 onready var dialog = $"/root/Main/Player/Camera2D/GUI/DialogBox"
 onready var player = $"/root/Main/Player"
+onready var sfx_open = $Open
+
 export(Array, String, MULTILINE) var lines = [""]
 
 var pulse = 0.0
@@ -12,6 +14,7 @@ func _process(_delta):
 	pulse += 0.1
 	material.set_shader_param("outline_color", Color(1, 1, 1, (sin(pulse) * 0.25 + 0.5) * glow_factor))
 	if Input.is_action_just_pressed("interact") && overlaps_body(player) && player.state == player.s.walk && player.sign_cooldown <= 0:
+		sfx_open.play()
 		player.switch_anim("back")
 		player.vel = Vector2.ZERO
 		player.sign_x = position.x
