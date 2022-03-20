40c57ab7140834216244cbbb09006bab02364df7
GTcreyon <plaidyoshi@gmail.com>
2022-03-20 12:26:42 +0000

    removed bloat and reorganised

--- classes/ld_item_class.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-class_name LDItem
-
-func _ready():
-	pass
--- enemies.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-var count = -1
-func grab_id():
-	count += 1
-	return count #ensures a unique ID for each entity
