b526fe45189a0ce04696a83f095e516ea5fd23d4
GTcreyon <plaidyoshi@gmail.com>
2022-04-08 20:32:20 +0100

    removed unnecessary code

--- actors/bg/t1/clouds_lower.gd
+++ actors/bg/t1/clouds_lower.gd
@@ -3,9 +3,5 @@ extends TextureRect
 onready var cam = $"/root/Main/Player/Camera2D"
 
 func _process(_delta):
-	var cam_pos = cam.get_camera_position()
-	var size = texture.get_size().x
-	margin_left = 0
-	#margin_top = max(-150, -150 - cam_pos.y / 20) * rect_scale.x
 	rect_scale = Vector2.ONE * max(round(OS.window_size.x / Singleton.DEFAULT_SIZE.x), 1)
 	rect_pivot_offset.y = 93 / 2
