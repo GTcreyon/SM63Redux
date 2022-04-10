ca0aff716c32c6fb9e28758dcd6eb2973eeed16d
GTcreyon <plaidyoshi@gmail.com>
2022-04-10 12:46:02 +0100

    fixed camera workaround and falling in transition

--- gui/pause/options/options.gd
+++ gui/pause/options/options.gd
@@ -2,5 +2,10 @@ extends Control
 
 onready var camera_fix = $Margin/CameraFix
 
+
+func _ready():
+	camera_fix.pressed = Singleton.disable_limits
+
+
 func _process(_delta):
 	Singleton.disable_limits = camera_fix.pressed
