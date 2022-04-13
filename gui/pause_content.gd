54861835cd541a61c8f1ba671366ab81c5c2d032
GTcreyon <plaidyoshi@gmail.com>
2022-04-13 15:44:51 +0100

    language box in Options

--- gui/pause_content.gd
+++ gui/pause_content.gd
@@ -12,22 +12,38 @@ onready var options_button = get_parent().get_node("ButtonOptions")
 onready var exit_button = get_parent().get_node("ButtonExit")
 
 func _process(_delta):
-	map.visible = false
-	fludd.visible = false
-	options.visible = false
-	exit.visible = false
-	info.visible = false
+	#control nodes don't like to be made invisible then visible in one frame, it messes with input
 	for node in stats:
 		node.visible = !Singleton.pause_menu
 	if map_button.pressed:
 		map.visible = true
+		fludd.visible = false
+		options.visible = false
+		exit.visible = false
+		info.visible = false
 	elif fludd_button.pressed:
+		map.visible = false
 		fludd.visible = true
+		options.visible = false
+		exit.visible = false
+		info.visible = false
 	elif options_button.pressed:
+		map.visible = false
+		fludd.visible = false
 		options.visible = true
+		exit.visible = false
+		info.visible = false
 	elif exit_button.pressed:
+		map.visible = false
+		fludd.visible = false
+		options.visible = false
 		exit.visible = true
+		info.visible = false
 	else:
+		map.visible = false
+		fludd.visible = false
+		options.visible = false
+		exit.visible = false
 		info.visible = true
 		for node in stats:
 			node.visible = true
