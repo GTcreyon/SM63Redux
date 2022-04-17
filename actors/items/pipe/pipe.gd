6b9271cb07fbd5d426d387cfaa8ba17843a8d2d7
GTcreyon <plaidyoshi@gmail.com>
2022-04-17 20:45:03 +0100

    various LD item fixes

--- actors/items/pipe/pipe.gd
+++ actors/items/pipe/pipe.gd
@@ -1,11 +1,5 @@
 extends StaticBody2D
 
-#P.S: thanks godot for not making it easy to handle things
-
-onready var player = $"/root/Main/Player"
-onready var player_sprite = $"/root/Main/Player/AnimatedSprite"
-onready var player_sfx = $"/root/Main/Player/Voice"
-
 onready var sound = $sfx #for sound effect
 
 var i = 0 #This is the variable that will increase until it reaches 60
@@ -13,55 +7,57 @@ var inc = false #this variable will serve to trigger the increment as a "delay"
 var can_warp = false #this variable is changed when mario enters the pipe's small area2D
 var slid = false #this is necessary to tell godot to change mario's Y position to slide down
 var store_state = 0
+var target = null
 
 export var target_x_pos = 0 #the x value where mario will get teleported
 export var target_y_pos = 0 #the y value where mario will get teleported
 
 func _process(_delta):
 	if slid:
-		if player.state == 7:
-			player.position.y = position.y
-			player.position.x = lerp(player.position.x, position.x, 0.75)
+		if target.state == 7:
+			target.position.y = position.y
+			target.position.x = lerp(target.position.x, position.x, 0.75)
 		else:
-			player.position.x = lerp(player.position.x, position.x, 0.25)
-			if player.position.y < position.y:
-				player.position.y += 0.7
+			target.position.x = lerp(target.position.x, position.x, 0.25)
+			if target.position.y < position.y:
+				target.position.y += 0.7
 	
-	if Input.is_action_pressed("down") && can_warp && store_state == player.s.walk && player.is_on_floor():
-		player_sfx.volume_db = -INF #dumb solution to mario making dive sounds
-		sound.play()
-		player.static_v = true #affects mario's whole input process
-		player_sprite.set_animation("front")
-		player.position = Vector2(lerp(player.position.x, position.x, 0.25), position.y - 30)
-		#warping will be disabled, then increment will start as mario slides down
-		can_warp = false
-		inc = true
-		slid = true
-	elif (player.state == player.s.pound_fall || player.state == player.s.pound_land) && can_warp:
-		sound.play()
-		player.static_v = true #affects mario's whole input process
-		#player.position = Vector2(position.x, position.y - 30)
-		player.position = Vector2(lerp(player.position.x, position.x, 0.75), position.y - 30)
-		
-		#warping will be disabled, then increment will start as mario slides down
-		can_warp = false
-		inc = true
-		slid = true
+	if can_warp:
+		if Input.is_action_pressed("down") && store_state == target.s.walk && target.is_on_floor():
+			target.get_node("Voice").volume_db = -INF #dumb solution to mario making dive sounds
+			sound.play()
+			target.static_v = true #affects mario's whole input process
+			target.get_node("Character").set_animation("front")
+			target.position = Vector2(lerp(target.position.x, position.x, 0.25), position.y - 30)
+			#warping will be disabled, then increment will start as mario slides down
+			can_warp = false
+			inc = true
+			slid = true
+		elif (target.state == target.s.pound_fall || target.state == target.s.pound_land):
+			sound.play()
+			target.static_v = true #affects mario's whole input process
+			#target.position = Vector2(position.x, position.y - 30)
+			target.position = Vector2(lerp(target.position.x, position.x, 0.75), position.y - 30)
+			
+			#warping will be disabled, then increment will start as mario slides down
+			can_warp = false
+			inc = true
+			slid = true
 	
-	store_state = player.state
+		store_state = target.state
 	
 	#the "delay" itself
 	if inc:
 		i += 1
 		
 	if i == 60: #mario then will be teleported as the "true" variables return to false
-		player_sfx.volume_db = -5
+		target.get_node("Voice").volume_db = -5
 		sound.stop()
-		player.position = Vector2(target_x_pos, target_y_pos)
-		player.static_v = false
-		player.switch_state(player.s.walk)
-		player.switch_anim("walk")
-		player.dive_correct(0)
+		target.position = Vector2(target_x_pos, target_y_pos)
+		target.static_v = false
+		target.switch_state(target.s.walk)
+		target.switch_anim("walk")
+		target.dive_correct(0)
 		i = 0
 		inc = false
 		slid = false
@@ -69,16 +65,11 @@ func _process(_delta):
 func _on_mario_top(body):
 	if body.global_position.y < global_position.y:
 		#print("on top")
-		if player.state == 0:
+		if body.state == body.s.walk:
 			can_warp = true
-		else:
-			can_warp = false
-		
-		if player.state == 4:
-			can_warp = false
-		else:
-			can_warp = true #for being able to work all the stuff above
+			target = body
 
 func _on_mario_off(_body):
 		#print("not on top")
 		can_warp = false #or else he won't
+		target = null
