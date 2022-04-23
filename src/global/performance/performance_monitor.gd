8e9f3fa4eec4336e27214bb3ae5df06f418e2b03
GTcreyon <67928545+GTcreyon@users.noreply.github.com>
2022-04-23 20:03:50 +0100

    removed process time monitor cuz it didn't work very well

--- src/global/performance/performance_monitor.gd
+++ src/global/performance/performance_monitor.gd
@@ -1,37 +1,25 @@
 extends Node
 
-const PHYSICS_DELTA: float = 1 / 60.0
-
 var data: Dictionary = {}
 var fps_lag: int = 0
-var process_lag: int = 0
 var physics_lag: int = 0
 var max_fps: int = 60
 var print_delay: int = 0
 var streak_id: int = 0
 
-func _process(delta):
+func _physics_process(delta):
 	if OS.get_ticks_msec() > 3000:
 		var fps = Performance.get_monitor(Performance.TIME_FPS)
-		var time_process = Performance.get_monitor(Performance.TIME_PROCESS)
 		var time_physics = Performance.get_monitor(Performance.TIME_PHYSICS_PROCESS)
 		
+		# warning-ignore:narrowing_conversion
 		max_fps = max(fps, max_fps)
-		if fps < max_fps:
+		if fps < max_fps - 1:
 			fps_lag += 1
 		else:
 			fps_lag = 0
 			
-		if Performance.get_monitor(Performance.TIME_PROCESS) > delta:
-			process_lag += 1
-		else:
-			if print_delay > 0:
-				print("Streak end: %d" % streak_id)
-			streak_id += 1
-			process_lag = 0
-			print_delay = 0
-			
-		if Performance.get_monitor(Performance.TIME_PHYSICS_PROCESS) > PHYSICS_DELTA:
+		if Performance.get_monitor(Performance.TIME_PHYSICS_PROCESS) > delta:
 			physics_lag += 1
 		else:
 			physics_lag = 0
@@ -39,20 +27,10 @@ func _process(delta):
 		if fps_lag % 60 == 10:
 			print("FPS lag. FPS:%d Max:%d" % [fps, max_fps])
 		
-		if process_lag == 10 || process_lag == 70 + print_delay:
-			print("Process lag. Loss:%f Delta:%f ID:%d" % [
-				(time_process - delta),
-				delta,
-				streak_id,
-				])
-			print_delay += 60
-			if process_lag >= 70:
-				process_lag = 11
-		
-		if physics_lag > 0:
+		if physics_lag % 60 == 1:
 			print("PHYSICS lag. Loss:%f Delta:%f" % [
-				(time_physics - PHYSICS_DELTA),
-				PHYSICS_DELTA
+				(time_physics - delta),
+				delta
 				])
 		if Input.is_action_just_pressed("dump"):
 			print_data()
