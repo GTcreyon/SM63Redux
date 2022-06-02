6066213615fb9e6ca8be7fad93db2ce06533ec70
Tricia <67928545+GTcreyon@users.noreply.github.com>
2022-06-02 13:36:59 +0100

    make it easier for the LD to interface with items

--- actors/items/log/log_fall.gd
+++ actors/items/log/log_fall.gd
@@ -8,7 +8,7 @@ onready var camera_area = $"/root/Main/CameraArea"
 onready var sprite = $Sprite
 onready var visibility = $VisibilityNotifier2D
 
-export var fall_timer : int = 60
+export var wait_time : int = 60
 
 var vel = Vector2.ZERO
 var falling : bool = false
@@ -22,10 +22,10 @@ func _ready():
 
 func _physics_process(_delta):
 	if falling:
-		if fall_timer > 0:
-			fall_timer -= 1
+		if wait_time > 0:
+			wait_time -= 1
 			sprite.position = Vector2((rng.randi() % JITTER) - JITTER / 2.0, (rng.randi() % JITTER) - JITTER / 2.0)
-		elif fall_timer <= 0:
+		elif wait_time <= 0:
 			sprite.position = Vector2.ZERO
 			vel.y += GRAVITY
 			position += vel
