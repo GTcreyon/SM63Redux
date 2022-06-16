4f00f15613fe93b0b7637e54f6d405189ebbac6c
Tricia <67928545+GTcreyon@users.noreply.github.com>
2022-06-16 22:53:52 +0100

    joypad stick motion

--- gui/pause/options/rebind_option.gd
+++ gui/pause/options/rebind_option.gd
@@ -53,7 +53,7 @@ func _ready():
 
 func _input(event):
 	if pressed:
-		if event is InputEventKey || event is InputEventJoypadButton:
+		if event is InputEventKey || event is InputEventJoypadButton || event is InputEventJoypadMotion:
 			InputMap.action_add_event(action_id, event)
 			unpress()
 			update_list()
@@ -72,6 +72,8 @@ func join_action_array(actions) -> String:
 				output += "(?)"
 			else:
 				output += JOYPAD_BUTTONS[action.button_index][get_brand_id()]
+		elif action is InputEventJoypadMotion:
+			output += get_joypad_motion_name(action.axis, action.axis_value)
 		else:
 			output += action.as_text()
 		output += ", "
@@ -121,3 +123,17 @@ func unpress():
 	pressed = false
 	action_name.add_color_override("font_color", Color.white)
 	key_list.add_color_override("font_color", Color.white)
+
+
+func get_joypad_motion_name(axis: int, value: int):
+	var output
+	match axis:
+		JOY_AXIS_0:
+			return "(Left Stick Left)" if value == -1 else "(Left Stick Right)"
+		JOY_AXIS_1:
+			return "(Left Stick Up)" if value == -1 else "(Left Stick Down)"
+		JOY_AXIS_2:
+			return "(Right Stick Left)" if value == -1 else "(Right Stick Right)"
+		JOY_AXIS_3:
+			return "(Right Stick Up)" if value == -1 else "(Right Stick Down)"
+			
