8cb0c9b36cda069651ed6922ab7e1760970e2f95
Tricia <67928545+GTcreyon@users.noreply.github.com>
2022-06-19 16:26:00 +0100

    reset binds to default

--- gui/pause/options/options_menu.gd
+++ gui/pause/options/options_menu.gd
@@ -1,35 +1,6 @@
 extends Control
 
 const SCROLL_SPEED = 16
-const WHITELISTED_ACTIONS = [
-	"left",
-	"right",
-	"jump",
-	"dive",
-	"spin",
-	"pound",
-	"fludd",
-	"switch_fludd",
-	"pause",
-	"interact",
-	"skip",
-	"zoom+",
-	"zoom-",
-	"semi",
-	"reset",
-	"timer_show",
-	"mute_music",
-	"mute_sfx",
-	"volume_music+",
-	"volume_music-",
-	"volume_sfx+",
-	"volume_sfx-",
-	"fullscreen",
-	"screen+",
-	"screen-",
-	"feedback",
-	"debug",
-]
 const PREFAB_REBIND_OPTION = preload("res://gui/pause/options/rebind_option.tscn")
 
 onready var list = $List
@@ -38,6 +9,7 @@ onready var touch_controls = $List/TouchControls
 onready var mute_music = $List/MuteMusic
 onready var mute_sfx = $List/MuteSFX
 onready var locale_select = $List/LocaleSelect
+onready var reset_binds = $List/ResetBinds
 var bus_music = AudioServer.get_bus_index("Music")
 var bus_sfx = AudioServer.get_bus_index("SFX")
 var height_set = false
@@ -56,7 +28,7 @@ func _ready():
 	$List/MuteSFX/Sprite.playing = mute_sfx.pressed
 	start_height = rect_size.y - list.margin_top + list.margin_bottom
 	height_set = true
-	for action in WHITELISTED_ACTIONS:
+	for action in Singleton.WHITELISTED_ACTIONS:
 		var inst = PREFAB_REBIND_OPTION.instance()
 		inst.action_id = action
 		inst.add_to_group("rebinds")
@@ -95,6 +67,7 @@ func manage_sizes():
 	touch_controls.rect_scale = Vector2.ONE * scale
 	mute_music.rect_scale = Vector2.ONE * scale
 	mute_sfx.rect_scale = Vector2.ONE * scale
+	reset_binds.rect_scale = Vector2.ONE * scale
 	#locale_select.rect_scale = Vector2.ONE * scale
 	#locale_select.margin_right = 0
 	for node in get_tree().get_nodes_in_group("rebinds"):
@@ -104,30 +77,9 @@ func manage_sizes():
 		touch_controls.rect_min_size = touch_controls.rect_min_size / prev_scale * scale
 		mute_music.rect_min_size = mute_music.rect_min_size / prev_scale * scale
 		mute_sfx.rect_min_size = mute_sfx.rect_min_size / prev_scale * scale
+		reset_binds.rect_min_size = reset_binds.rect_min_size / prev_scale * scale
 		#locale_select.rect_min_size.y = 32# * scale
 		for node in get_tree().get_nodes_in_group("rebinds"):
 			node.rect_min_size.y = node.rect_min_size.y / prev_scale * scale
 			
 		prev_scale = scale
-
-
-func save_input_map():
-	var save_dict = {}
-	for key in InputMap.get_actions():
-		if WHITELISTED_ACTIONS.has(key):
-			for action in InputMap.get_action_list(key):
-				if !save_dict.has(key):
-					save_dict[key] = []
-				var key_entry = save_dict[key]
-				match action.get_class():
-					"InputEventKey":
-						key_entry.append("k:%d" % action.scancode)
-					"InputEventJoypadButton":
-						key_entry.append("b:%d" % action.button_index)
-					"InputEventJoypadMotion":
-						key_entry.append("a:%d;%d" % [action.axis, action.axis_value])
-	var content = to_json(save_dict)
-	var file = File.new()
-	file.open("user://controls.json", File.WRITE)
-	file.store_string(content) # minimise the amount of time spent with the file open
-	file.close()
