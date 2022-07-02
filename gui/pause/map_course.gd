c6ba06bc487ec5548a6465a44d1967aa798bde11
RandomCatDude <kud.stanislav@gmail.com>
2022-07-02 16:43:00 +0500

    protect against division-by-zero error

--- gui/pause/map_course.gd
+++ gui/pause/map_course.gd
@@ -5,6 +5,10 @@ onready var hover = $FluddGroup/Hover
 onready var turbo = $FluddGroup/Turbo
 
 func resize():
+	# protect against division-by-zero error i ran into on win7
+	# caused by minimizing the window using the button in the right edge of the taskbar
+	if OS.window_size.y == 0: return
+	
 	if OS.window_size.x / OS.window_size.y > 1.474:
 		coin_group.columns = 6
 		coin_group.margin_top = 90
