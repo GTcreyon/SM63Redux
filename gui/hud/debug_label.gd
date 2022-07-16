50ebc0650dc7f0e5d083f7757dcaf201d6eca95c
Tricia <67928545+GTcreyon@users.noreply.github.com>
2022-07-16 23:06:43 +0100

    move some gui stuff and remove some bloat

--- gui/hud/debug_label.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-func _process(_delta):
-	text = "Classic: " + ("On" if $"/root/Singleton".classic else "Off")
-	#text = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!?'\""
-	#text = String(get_parent().get_parent().position)
