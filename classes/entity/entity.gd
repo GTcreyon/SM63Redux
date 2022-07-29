9b84c716334a8d7e1decec42063e49ce4d1ad0d6
Tricia <67928545+GTcreyon@users.noreply.github.com>
2022-07-29 13:01:47 +0100

    more rearranging

---
+++ classes/entity/entity.gd
@@ -0,0 +1,57 @@
+class_name Entity
+extends KinematicBody2D
+# Root class for all entities that move in any way.
+# Entities have the Entity collision layer bit enabled, so they can influence weights.
+
+const GRAVITY = 0.17
+const TERM_VEL_AIR = 6
+const TERM_VEL_WATER = 2
+
+export var disabled = false setget set_disabled
+var vel = Vector2.ZERO
+var _water_bodies: int = 0
+
+
+func _get_configuration_warning():
+	pass
+
+
+func _physics_process(_delta):
+	_physics_step()
+
+
+func _wall_contact():
+	pass
+
+
+func _physics_step():
+	if !disabled:
+		if _water_bodies > 0:
+			vel.y = min(vel.y + GRAVITY, TERM_VEL_WATER)
+		else:
+			vel.y = min(vel.y + GRAVITY, TERM_VEL_AIR)
+		
+		if is_on_floor():
+			if is_on_wall():
+				_wall_contact()
+		
+		var snap
+		if is_on_floor():
+			snap = Vector2(0, 4)
+		else:
+			snap = Vector2.ZERO
+		
+		#warning-ignore:RETURN_VALUE_DISCARDED
+		move_and_slide_with_snap(vel * 60, snap, Vector2.UP, true)
+
+
+func _on_WaterCheck_area_entered(_area):
+	_water_bodies += 1
+
+
+func _on_WaterCheck_area_exited(_area):
+	_water_bodies -= 1
+
+
+func set_disabled(val):
+	disabled = val
