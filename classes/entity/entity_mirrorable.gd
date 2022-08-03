5f6e24b997f75a604a96f2a83d590c5f18f0588a
Tricia <67928545+GTcreyon@users.noreply.github.com>
2022-08-03 16:46:15 +0100

    fix EntityMirrorable not obeying override protocol

--- classes/entity/entity_mirrorable.gd
+++ classes/entity/entity_mirrorable.gd
@@ -1,16 +1,21 @@
 class_name EntityMirrorable
 extends Entity
-# Root class for an entity that can have its visuals mirrored
+# Parent class for an entity that can have its visuals mirrored.
 
 export var mirror: bool = false
+
 export var _sprite_path: NodePath = "AnimatedSprite"
 onready var sprite = get_node_or_null(_sprite_path)
 
 
 func _process(_delta):
-	_manage_mirror()
+	_entity_mirrorable_process()
 
 
-func _manage_mirror():
+func _entity_mirrorable_process():
 	if sprite != null:
 		sprite.flip_h = mirror
+
+
+func _process_override(_delta):
+	_entity_mirrorable_process()
