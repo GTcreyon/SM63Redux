b5456814acaccfe0dad5ee82aeca036d6b0884c4
jaschutte <34577095+jaschutte@users.noreply.github.com>
2022-08-21 13:52:21 +0200

    vps functions & vectors

--- level_designer/visual_pipescript/node_placer.gd
+++ level_designer/visual_pipescript/node_placer.gd
@@ -16,7 +16,10 @@ func get_unique_variable_name():
 	id += 1
 	return "UNIQUE_VAR#%d#" % id
 
-func compile_to_source(start_piece):
+func add_line(txt, prefix):
+	source_code += prefix + txt + "\n"
+
+func compile_to_source(start_piece, prefix = ""):
 	if !start_piece:
 		return
 	
@@ -25,35 +28,75 @@ func compile_to_source(start_piece):
 		var piece = queue.pop_front()
 		
 		match piece.json_data.type:
-			"if":
-				var condition = get_unique_variable_name()
-				source_code += "calc %s %s\n" % [condition, piece.get_input_text(0)]
-				source_code += "if %s\n" % condition
-				compile_to_source(piece.inner_connection)
-				source_code += "end\n"
-			"loop":
-				var condition = get_unique_variable_name()
-				var label = get_unique_variable_name()
-				source_code += "label %s\n" % label
-				source_code += "calc %s %s\n" % [condition, piece.get_input_text(0)]
-				source_code += "if %s\n" % condition
-				compile_to_source(piece.inner_connection)
-				source_code += "goto %s\n" % label
-				source_code += "end\n"
-			"set":
-				source_code += "calc %s %s\n" % [piece.get_input_text(0), piece.get_input_text(1)]
-			"print":
-				source_code += "print %s\n" % piece.get_input_text(0)
 			"start":
 				pass
 			"exit":
-				source_code += "exit!\n"
+				add_line("exit!", prefix)
+			"if":
+				var condition = get_unique_variable_name()
+				add_line("calc %s %s" % [condition, piece.get_input_text(0)], prefix)
+				add_line("if %s\n" % condition, prefix)
+				compile_to_source(piece.inner_connection, prefix + "\t")
+				add_line("end", prefix)
+			"loop":
+				var condition = get_unique_variable_name()
+				var label = get_unique_variable_name()
+				add_line("label %s" % label, prefix)
+				add_line("calc %s %s" % [condition, piece.get_input_text(0)], prefix)
+				add_line("if %s" % condition, prefix)
+				compile_to_source(piece.inner_connection, prefix + "\t")
+				add_line("goto %s" % label, prefix)
+				add_line("end", prefix)
+			"set":
+				add_line("calc %s %s" % [piece.get_input_text(0), piece.get_input_text(1)], prefix)
+			"create-vector":
+				var var_name = piece.get_input_text(0)
+				add_line("gd-vec2-new %s %s %s" % [var_name, piece.get_input_text(1), piece.get_input_text(2)], prefix)
+				add_line("gd-vec2-get %s %s x.S" % [var_name + ".x", var_name], prefix)
+				add_line("gd-vec2-get %s %s y.S" % [var_name + ".y", var_name], prefix)
+			"unwrap-vector":
+				var var_name = piece.get_input_text(0)
+				add_line("gd-vec2-get %s %s x.S" % [var_name + ".x", var_name], prefix)
+				add_line("gd-vec2-get %s %s y.S" % [var_name + ".y", var_name], prefix)
+			"update-vector":
+				var var_name = piece.get_input_text(0)
+				add_line("gd-vec2-set %s %s x.S" % [var_name + ".x", var_name], prefix)
+				add_line("gd-vec2-set %s %s y.S" % [var_name + ".y", var_name], prefix)
+			"print":
+				add_line("print %s" % piece.get_input_text(0), prefix)
 			"function":
-				source_code += "function %s\n" % piece.get_input_text(0)
-				compile_to_source(piece.bottom_connection)
-				source_code += "return\n"
+				add_line("function %s" % piece.get_input_text(0), prefix)
+				compile_to_source(piece.bottom_connection, prefix + "\t")
+				add_line("return", prefix)
 			"call":
-				source_code += "call %s\n" % piece.get_input_text(0)
+				add_line("call %s" % piece.get_input_text(0), prefix)
+			"gd-call":
+				var args = []
+				for editor in piece.line_edits:
+					if editor.text.empty():
+						printerr("%s is not filled in!" % name)
+					args.append(editor.text)
+				add_line(
+					("gd-call" + " %s".repeat(len(args))) % args,
+					prefix
+				)
+			"gd-call-return":
+				var args = [
+					piece.get_input_text(2),
+					piece.get_input_text(0),
+					piece.get_input_text(1)
+				]
+				for editor_idx in len(piece.line_edits):
+					if editor_idx < 3:
+						continue
+					var editor = piece.line_edits[editor_idx]
+					if editor.text.empty():
+						printerr("%s is not filled in!" % name)
+					args.append(editor.text)
+				add_line(
+					("gd-call-set" + " %s".repeat(len(args))) % args,
+					prefix
+				)
 			_:
 				printerr("Unknown piece ", piece.json_data.type)
 		
@@ -134,12 +177,11 @@ func _on_Run_pressed():
 	# If we found a starters node, then compile from there too
 	if start_node:
 		compile_to_source(start_node)
-
 		print("-- SRC --")
 		print(source_code)
 		print("--     --")
-		
 		pipescript.interpret(source_code)
+		pipescript.set_object_variable("self", get_parent().get_child(1).get_child(0))
 		pipescript.execute()
 		
 #	pipescript.interpret("""
@@ -153,6 +195,5 @@ func _on_Run_pressed():
 #gd-vec2-set new_pos_x new_pos x.S
 #gd-call button set_position.S new_pos
 #""")
-#	pipescript.set_object_variable("self", get_node("Test1"))
 #	pipescript.set_object_variable("button", get_parent().get_child(1).get_child(0))
 #	pipescript.execute()
