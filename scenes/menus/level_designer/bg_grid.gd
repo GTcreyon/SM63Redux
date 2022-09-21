f7c4eebedaa0b4aa73fd2e006f322b72d4045726
jaschutte <34577095+jaschutte@users.noreply.github.com>
2022-09-21 20:49:59 +0200

    Clean slate for selection

--- scenes/menus/level_designer/bg_grid.gd
+++ scenes/menus/level_designer/bg_grid.gd
@@ -1,4 +1,9 @@
 extends TextureRect
 
+onready var camera: Camera2D = $"/root/Main/Camera"
+
 func _ready():
-	$"../BGParent".modulate = Color(0.5, 0.5, 0.5)
+	$"../BGParent".modulate = Color(0.7, 0.7, 0.7)
+
+func _process(_dt):
+	material.set_shader_param("camera_position", camera.position)
