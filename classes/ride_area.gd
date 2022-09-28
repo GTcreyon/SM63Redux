6169fcb0dc0b3a9f06b76efc6fc95ab7fb888013
GTcreyon <67928545+GTcreyon@users.noreply.github.com>
2022-09-28 22:30:35 +0100

    Update ride area class and apply to falling log

--- classes/ride_area.gd
+++ classes/ride_area.gd
@@ -1,7 +1,9 @@
 class_name RideArea
 extends Area2D
+# Area that detects bodies standing within it. Detects feet if the body has them.
+# A "rider" is a body that is standing on ground within the area.
 
-
+# Returns an array of detected riding bodies.
 func get_riding_bodies() -> Array:
 	var riders = []
 	for body in get_overlapping_bodies():
@@ -10,9 +12,15 @@ func get_riding_bodies() -> Array:
 	return riders
 
 
+# Returns true if a rider is present.
+func has_rider() -> bool:
+	return !get_riding_bodies().empty()
+
+
 # If the body does not have a feet area, return true.
 # Else, check if we overlap the feet area.
 # This allows us to be accurate for entities with defined feet, but fall back on their whole body if needed.
 func _check_feet(body) -> bool:
 	var feet = body.get("feet_area")
 	return feet == null or overlaps_area(feet)
+
