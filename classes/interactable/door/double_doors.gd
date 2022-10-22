62fe79916deb13a0c655936cff85f5f5d0b4e274
Koopa1018 <caleblovestoytrains@gmail.com>
2022-10-22 03:48:14 -0600

    Make double doors one object
    double_doors.gd is a convenience script that gives its child objects its own properties (with offsets relative to which side they're on). This way there's only one set of properties to configure, rather than setting it individually on both doors.

--- classes/interactable/door/double_doors.gd
+++ classes/interactable/door/double_doors.gd
@@ -1,38 +1,49 @@
 tool
 extends Node2D
 
-const SINGLE_DOOR_OFFSET = 8
+const SINGLE_DOOR_OFFSET = Vector2(8,0)
 
 export var target_pos = Vector2.ZERO setget set_target_pos, get_target_pos
 export var move_to_scene = false setget set_move_to_scene, get_move_to_scene
 export var scene_path : String setget set_scene_path, get_scene_path
 
-onready var door_l = $"Door L"
-onready var door_r = $"Door_R"
+onready var door_l : Door
+onready var door_r : Door
 
 
 func set_target_pos(val):
-	door_l.target_pos.x = val.x - SINGLE_DOOR_OFFSET
-	door_r.target_pos.x = val.x + SINGLE_DOOR_OFFSET
+	door_l = $Door_L
+	door_r = $Door_R
+	
+	door_l.target_pos = val - SINGLE_DOOR_OFFSET
+	door_r.target_pos = val + SINGLE_DOOR_OFFSET
 
 
 func set_move_to_scene(val):
+	door_l = $Door_L
+	door_r = $Door_R
+	
 	door_l.move_to_scene = val
 	door_r.move_to_scene = val
 
 
 func set_scene_path(val):
+	door_l = $Door_L
+	door_r = $Door_R
+	
 	door_l.scene_path = val
 	door_r.scene_path = val
 
 
 func get_target_pos():
-	return door_l.target_pos + Vector2(SINGLE_DOOR_OFFSET,0)
+	return door_l.target_pos + SINGLE_DOOR_OFFSET
 
 
 func get_move_to_scene():
+	door_l = $Door_L
 	return door_l.move_to_scene
 
 
 func get_scene_path():
+	door_l = $Door_L
 	return door_l.scene_path
