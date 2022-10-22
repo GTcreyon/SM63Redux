f7eaec76020d1e96aa612d97c7798761c056f430
Koopa1018 <caleblovestoytrains@gmail.com>
2022-10-22 00:22:40 -0600

    Doors now on par with pipes
    Doors extending Interactable led to incorrect behavior where activating a door was done by pressing Z instead of up. This update ports code from pipes to make doors hopefully work as intended.
    
    Because pipes in turn had scene change features ported over from doors, this means doors and pipes are now feature-identical (subject to testing, of course).

--- classes/interactable/door/door.gd
+++ classes/interactable/door/door.gd
@@ -1,28 +1,60 @@
 class_name Door
-extends Interactable
+extends Area2D
+
+const ENTER_LENGTH = 60
+
+const TRANSITION_SPEED_IN = 15
+const TRANSITION_SPEED_OUT = 15
 
 export var target_pos: Vector2
 export var move_to_scene = false
-export var next_scene: PackedScene
+export var scene_path : String
 
 var timer = 0
-var entered = false
-var body_entering = null
+var entering = false
+var can_warp = false
+var target = null
+var store_state = 0
+
+onready var sweep_effect = $"/root/Singleton/WindowWarp"
 
 func _physics_process(_delta):
-	if entered == true:
+	if entering:
+		# Do entering animation
+		pass
+	
+	if can_warp:
+		# Begin entering door if up is pressed (while grounded + standing still)
+		if Input.is_action_pressed("up") and store_state == target.S.NEUTRAL and target.is_on_floor():
+			target.locked = true
+			$DoorSprite.play("opening")
+			entering = true
+		
+	# Tick the animation timer
+	if entering == true:
 		timer += 1
-	if timer == 60:
+		
+	# When the timer rings, warp Mario
+	if timer == ENTER_LENGTH:
 		if move_to_scene == true:
-			# warning-ignore:return_value_discarded
-			get_tree().change_scene_to(next_scene)
-		body_entering.position = target_pos
-		body_entering.locked = false
-		entered = false
+			sweep_effect.warp(target_pos, scene_path, TRANSITION_SPEED_IN, TRANSITION_SPEED_OUT)
+		else:
+			# teleport Mario within the level
+			target.position = target_pos
+			
+			# reset Mario to normal
+			target.locked = false
+			
+			# Reset door to normal
+			entering = false
 
 
-func _interact_with(body) -> void:
-	body.locked = true
-	body_entering = body
-	sprite.play("opening")
-	entered = true
+func _on_mario_touch(body):
+	if body.state == body.S.NEUTRAL:
+		can_warp = true
+		target = body
+
+
+func _on_mario_off(_body):
+	can_warp = false # Or else he won't
+	target = null
