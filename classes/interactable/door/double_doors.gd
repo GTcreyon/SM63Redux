6d7da3cf8951d7f31ee3a69be67753f036188fcd
Koopa1018 <caleblovestoytrains@gmail.com>
2022-10-24 01:36:41 -0600

    Double doors finally work
    Ditched the tool approach. Just had it populate the child doors at ready time.

--- classes/interactable/door/double_doors.gd
+++ classes/interactable/door/double_doors.gd
@@ -3,48 +3,24 @@ extends Node2D
 
 const SINGLE_DOOR_OFFSET = Vector2(8,0)
 
-export var target_pos = Vector2.ZERO setget set_target_pos, get_target_pos
-export var move_to_scene = false setget set_move_to_scene, get_move_to_scene
-export var scene_path : String setget set_scene_path, get_scene_path
+export var target_pos = Vector2.ZERO
+export var move_to_scene = false
+export var scene_path : String
 
 onready var door_l : Door = $Door_L
 onready var door_r : Door = $Door_R
 
-
-func set_target_pos(val):
-	door_l = $Door_L
-	door_r = $Door_R
-	
-	door_l.target_pos = val - SINGLE_DOOR_OFFSET
-	door_r.target_pos = val + SINGLE_DOOR_OFFSET
-
-
-func set_move_to_scene(val):
-	door_l = $Door_L
-	door_r = $Door_R
-	
-	door_l.move_to_scene = val
-	door_r.move_to_scene = val
-
-
-func set_scene_path(val):
-	door_l = $Door_L
-	door_r = $Door_R
-	
-	door_l.scene_path = val
-	door_r.scene_path = val
-
-
-func get_target_pos():
-	door_l = $Door_L
-	return door_l.target_pos + SINGLE_DOOR_OFFSET
-
-
-func get_move_to_scene():
-	door_l = $Door_L
-	return door_l.move_to_scene
-
-
-func get_scene_path():
-	door_l = $Door_L
-	return door_l.scene_path
+func _ready():
+	#When run in game, populate child doors with my data
+	if !Engine.editor_hint:
+		door_l = $Door_L
+		door_r = $Door_R
+		
+		door_l.target_pos = target_pos - SINGLE_DOOR_OFFSET
+		door_r.target_pos = target_pos + SINGLE_DOOR_OFFSET
+		
+		door_l.move_to_scene = move_to_scene
+		door_r.move_to_scene = move_to_scene
+		
+		door_l.scene_path = scene_path
+		door_r.scene_path = scene_path
