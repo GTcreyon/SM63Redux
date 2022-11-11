0bb20518d87746c133820f755587c6e859c22a44
Koopa1018 <caleblovestoytrains@gmail.com>
2022-11-10 21:29:37 -0700

    Total rewrite of double doors
    Double doors no longer *contain* single doors--instead, they replicate their behavior, with some extra functionality to choose which door Mario goes into. (That functionality's actually a little bit complicated--and it's why I wanted to make this change in the first place.)

--- classes/interactable/door/double_doors.gd
+++ classes/interactable/door/double_doors.gd
@@ -1,30 +1,64 @@
-extends Node2D
+extends InteractableWarp
 
-const SINGLE_DOOR_OFFSET = Vector2(8,0)
+const CENTERING_SPEED = Door.CENTERING_SPEED
+const SINGLE_DOOR_OFFSET = 8
+const FACING_BIAS = 2 # If Mario's facing one door, he can enter it this many pixels further away.
 
-export var sprite : SpriteFrames
-export var target_pos = Vector2.ZERO
-export var move_to_scene = false
-export var scene_path : String
+export var door_graphic : SpriteFrames
 
-onready var door_l : Door = $Door_L
-onready var door_r : Door = $Door_R
+# This stores which side we've chosen to enter.
+# We need this because Mario's facing direction shifts as he enters
+# a door, meaning we can't rely on "is he left of the doors' origin?"
+var chosen_side = 0
+
+func _draw():
+	# For debugging, render the split point for which door Mario will enter.
+	draw_line(global_position + mario_offset(),
+		global_position + mario_offset() + Vector2(0, 2),
+		Color(0.1, 1.0, 0.2))
 
 func _ready():
-	# When run in game, populate child doors with my data
-	if !Engine.editor_hint:
-		door_l = $Door_L
-		door_r = $Door_R
-		
-		$Door_L/DoorSprite.frames = sprite
-		$Door_R/DoorSprite.frames = sprite
-		
-		door_l.target_pos = target_pos - SINGLE_DOOR_OFFSET
-		door_r.target_pos = target_pos + SINGLE_DOOR_OFFSET
-		
-		door_l.move_to_scene = move_to_scene
-		door_r.move_to_scene = move_to_scene
-		
-		door_l.scene_path = scene_path
-		door_r.scene_path = scene_path
+	$SpriteL.frames = door_graphic
+	$SpriteR.frames = door_graphic
 
+
+func _update_animation(_frame: int, _mario):
+	# Gradually center Mario over one of the doors
+	_mario.global_position.x = lerp(_mario.global_position.x,
+		global_position.x + mario_offset().x, CENTERING_SPEED)
+	
+	if _frame == 0:
+		# Start Mario's enter animation
+		_mario.get_node("Character").set_animation("back")
+		# Start door opening animation on the appropriate door
+		if should_use_right_door() == true:
+			$SpriteR.play("opening")
+			chosen_side = 1
+		else:
+			$SpriteL.play("opening")
+			chosen_side = -1
+
+
+func _animation_length():
+	return 60
+
+
+func _begin_scene_change(target_pos, scene_path):
+	# Clobber the destination position so Mario comes out on the left or right.
+	._begin_scene_change(target_pos + mario_offset(), scene_path)
+
+
+func mario_offset() -> Vector2:
+	return Vector2(SINGLE_DOOR_OFFSET * chosen_side, 0)
+
+
+func should_use_right_door() -> bool:
+	# Broadly, this checks if Mario is to the right or left of the center.
+	# BUT--if Mario is facing one direction or another, he'll be SLIGHTLY more
+	# receptive to using the door he's facing towards.
+	if mario.sprite.flip_h == false:
+		# Mario facing right.
+		return mario.global_position.x > (global_position.x - FACING_BIAS)
+	else:
+		# Mario facing left.
+		return mario.global_position.x >= (global_position.x + FACING_BIAS)
