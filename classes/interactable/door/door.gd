21e403e8355b7cc2c3e98a708e05acf2fc3de3d7
Koopa1018 <caleblovestoytrains@gmail.com>
2022-11-10 19:21:36 -0700

    Refine InteractableExit for ease of use
    At Creyon's suggestion, switched InteractableExit from using copied Pipe code (apparently dated) to directly extending Interactable (HOLY dang it's so much more elegant).
    
    While I was at it, I also did some major refactors to make the class easy to inherit from--refactor timer-and-state system into a unified timer-only system that handles transition timing and stopping on its own, meaning people inheriting this class just don't have to worry about that--they just build out the animation, set its length, and be done with it.

--- classes/interactable/door/door.gd
+++ classes/interactable/door/door.gd
@@ -1,38 +1,23 @@
 class_name Door
 extends InteractableExit
 
-const ENTER_LENGTH = 60
-const ENTER_BEGIN_TRANSITION = ENTER_LENGTH - TRANSITION_SPEED_IN
 const CENTERING_SPEED = 0.25
 
-export var sprite : SpriteFrames
+export var doorGraphic : SpriteFrames
 
 func _ready():
-	$DoorSprite.frames = sprite
+	$DoorSprite.frames = doorGraphic
 
 
 func _update_animation(_frame: int, _mario):
 	# Gradually center Mario
 	_mario.global_position.x = lerp(_mario.global_position.x, global_position.x, CENTERING_SPEED)
 	
-	match _frame:
-		ENTER_BEGIN_TRANSITION:
-			#Play transition animation
-			return EnterState.CAN_TRANSITION
-		ENTER_LENGTH:
-			return EnterState.DONE
-		0:
-			# Start Mario's enter animation
-			_mario.get_node("Character").set_animation("back")
-			# Start door opening animation
-			$DoorSprite.play("opening")
-			
-			return EnterState.ENTERING
-		_:
-			return EnterState.ENTERING
+	if _frame == 0:
+		# Start Mario's enter animation
+		_mario.get_node("Character").set_animation("back")
+		# Start door opening animation
+		$DoorSprite.play("opening")
 
-
-func _scene_transition(target_pos, scene_path):
-	# Default warp transition is a star iris.
-	var sweep_effect = $"/root/Singleton/WindowWarp"
-	sweep_effect.warp(target_pos, scene_path, TRANSITION_SPEED_IN, TRANSITION_SPEED_OUT)
+func _animation_length():
+	return 60
--- classes/interactable/interactable_exit.gd
+++ classes/interactable/interactable_exit.gd
@@ -1,12 +1,21 @@
 class_name InteractableExit
-extends Area2D
-
-enum EnterState {
-	NONE,
-	ENTERING,
-	CAN_TRANSITION,
-	DONE,
-}
+extends Interactable
+# A variant of Interactable that warps Mario somewhere,
+# either to another scene or someplace in the current one.
+# 
+# To implement this base class, there are two functions to extend:
+# - _animation_length() returns the expected duration of the animation
+#   as an int.
+# - _update_animation(_frame, _mario) updates the enter animation for the
+#   given frame.
+# For further customization, you can override _interact_check() if you want
+# it to respond to a different button than "up",
+# or override _begin_warp(target_pos, scene_pos) to change what transition
+# will be used on scene change (star iris out by default).
+# 
+# Please note that if the particular warp is set to move to a different scene,
+# the exit transition will begin TRANSITION_SPEED_IN frames before the end of
+# the animation.
 
 const TRANSITION_SPEED_IN = 25
 const TRANSITION_SPEED_OUT = 15
@@ -15,66 +24,71 @@ export var target_pos = Vector2.ZERO
 export var move_to_scene = false
 export var scene_path : String
 
-var target = null # This is Mario
-var anim_timer = 0 # This goes up by one every frame of the animation
-var enter_state = EnterState.NONE # This indicates what phase of animation we're in
-var can_warp = false # This becomes true when Mario touches this (reset to false on enter)
-var store_state = 0 # This is Mario's current state, updated after begin-warp check
+var mario = null # This holds a reference to Mario during the animation
+var anim_timer = -1 # This goes down by one every frame of the animation
 
-func _physics_process(_delta):
-	if can_warp and target.locked == false:
-		# Begin entering door if up is pressed (while grounded + standing still)
-		if Input.is_action_pressed("up") and store_state == target.S.NEUTRAL and target.is_on_floor():
-			# Lock Mario's input so he can't be controlled
-			target.locked = true
-			
-			# Change to "animating" state
-			can_warp = false
-			enter_state = EnterState.ENTERING
-		
-		store_state = target.state # for next frame
+
+# When interacted, go into play-animation state
+func _interact_with(body):
+	mario = body
 	
-	if enter_state != EnterState.NONE:
-		# Run animation
-		enter_state = _update_animation(anim_timer, target)
-		# Tick the animation timer
-		anim_timer += 1
-		
-	# Begin scene-change transition if the animation is ready for it
-	if (enter_state == EnterState.CAN_TRANSITION or enter_state == EnterState.DONE) \
-		and move_to_scene == true:
-		_scene_transition(target_pos, scene_path)
-		
-	# If not changing scenes, warp Mario on anim_timer ring
-	if enter_state == EnterState.DONE and move_to_scene != true:
-		# teleport Mario within the level
-		target.position = target_pos
-		
-		# reset Mario to normal
-		target.locked = false
-		
-		# Reset door to normal
-		anim_timer = 0
-		enter_state = EnterState.NONE
+	# Lock Mario's input so he can't be controlled
+	mario.locked = true
+	
+	# Change to "animating" state
+	anim_timer = _animation_length()
 
 
-func _on_mario_touch(body):
-	if body.state == body.S.NEUTRAL:
-		can_warp = true
-		target = body
+func _physics_override():
+	._physics_override()
+	
+	# Run frame-by-frame animation logic.
+	if anim_timer > -1:
+		# Step the animation one frame forward.
+		# Pass it an inverted timer so people inheriting this class can work
+		# with frames-elapsed instead of frames-left.
+		_update_animation(_animation_length() - anim_timer, mario)
+		
+		# Begin scene-change transition if the animation is ready
+		if anim_timer == min(TRANSITION_SPEED_IN, _animation_length()) and move_to_scene == true:
+			_begin_warp(target_pos, scene_path)
+			
+		# If not changing scenes, warp Mario when the timer rings
+		if anim_timer == 0 and move_to_scene != true:
+			mario.position = target_pos
+			mario.locked = false
+			
+			# No longer need this reference, let's drop it.
+			mario = null
+		
+		# Tick the animation timer.
+		# This is also what stops the timer when it runs out--
+		# when the timer is 0 and Mario warps, the timer will
+		# tick down to -1 (the "not running" value) and stop.
+		anim_timer -= 1
 
 
-func _on_mario_off(_body):
-	if enter_state == EnterState.NONE: # w/o this check, target will get nulled when animation ends
-		can_warp = false # Or else he won't
-		target = null
+# Checks if Mario is in a state where he can interact
+func _state_check(body) -> bool:
+	return !body.locked and ._state_check(body) and body.is_on_floor()
+
+
+# Checks if the trigger button was pressed
+func _interact_check() -> bool:
+	# Use Up by default, not the read-sign button (can be changed later)
+	return Input.is_action_just_pressed("up")
+
+
+func _animation_length() -> int:
+	return 0
 
 
 func _update_animation(_frame: int, _mario):
-	return EnterState.DONE
+	pass
 
 
-func _scene_transition(target_pos, scene_path):
-	# Default warp transition is a star iris.
+# Begins the exit transition. Called TRANSITION_SPEED_IN frames BEFORE the animation ends!
+func _begin_warp(target_pos, scene_path):
+	# Default warp transition is a star iris
 	var sweep_effect = $"/root/Singleton/WindowWarp"
 	sweep_effect.warp(target_pos, scene_path, TRANSITION_SPEED_IN, TRANSITION_SPEED_OUT)
