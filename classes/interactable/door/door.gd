37195a39496da4b14b6c47200a29766b7e08fc2c
Koopa1018 <caleblovestoytrains@gmail.com>
2022-11-14 22:22:09 -0700

    Fix bug where door loses sprite on scene load
    It was very annoying. When you'd open the scene in the editor, the door's Sprite field would get unset and the game would refuse to run until you manually reverted it to the default (set) value.
    
    Problem came from running the script as tool, it turns out. At present, Interactable (the base-st version) has an `onready` variable that assigns the sprite field in some weird hootenany that I don't understand.
    
    IIRC Creyon thinks that part of Interactable could be moved to InteractableDialog, but because this is a door PR, I'd rather not touch that part. As such, here's a fix that just gets rid of the door's tool status and moves the graphic's setget behavior into _ready().

--- classes/interactable/door/door.gd
+++ classes/interactable/door/door.gd
@@ -1,18 +1,18 @@
-tool
 class_name Door
 extends InteractableWarp
 
 const CENTERING_SPEED = 0.25
 
-export var door_graphic : SpriteFrames setget set_door_graphic
+export var door_graphic : SpriteFrames
 
 var player_fade_start_time : int
 var player_fade_rate : float
 var door_close_start_time : int
 
-func set_door_graphic(graphic : SpriteFrames):
-	door_graphic = graphic
+func _ready():
+	_set_sprite_frames(door_graphic)
 	
+	# Load animation timings
 	if door_graphic is DoorSkin:
 		# Resource has animation timings saved in it; load them.
 		player_fade_start_time = door_graphic.player_fade_start_time
@@ -23,10 +23,6 @@ func set_door_graphic(graphic : SpriteFrames):
 		player_fade_start_time = DoorSkin.DEFAULT_PLAYER_FADE_START
 		player_fade_rate = DoorSkin.DEFAULT_PLAYER_FADE_RATE
 		door_close_start_time = DoorSkin.DEFAULT_DOOR_CLOSE_START
-
-
-func _ready():
-	_set_sprite_frames(door_graphic)
 
 
 func _update_animation(_frame: int, _player):
