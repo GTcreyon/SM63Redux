7702c0af9ffd51be75ffb53266f3765a39992032
Koopa1018 <caleblovestoytrains@gmail.com>
2022-11-27 00:50:47 -0700

    Begin visual version of the locked warp
    Not very visual yet. Currently when locked, it "interacts" by quietly doing nothing.

--- classes/interactable/locked_warp/locked_warp_visual.gd
+++ classes/interactable/locked_warp/locked_warp_visual.gd
@@ -1,16 +1,28 @@
 extends Interactable
 
+export(NodePath) onready var warp = get_node(warp) as InteractableWarp
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var anim_timer = -1
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Validate child warp object.
+	assert(warp)
+	
+	# Disable the child warp so it doesn't get used by mistake.
+	warp.disabled = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _interact_check() -> bool:
+	# Passthrough to inner warp's interact check.
+	return warp._interact_check()
+
+
+func _interact_with(body):
+	if true: # has key
+		# TODO: Unlock sequence.
+		
+		# Passthrough to inner warp's interaction.
+		warp._interact_with(body)
+	else:
+		# Has not key. Dp animation.
+		pass
