3c1fc2a305dd58111a3779322800f0429490107f
Koopa1018 <caleblovestoytrains@gmail.com>
2022-11-28 08:41:44 -0700

    Comment flag_server
    Some parts really are self-explanatory; comments were only added to explain the slightly more inscrutable parts.

--- classes/global/flag_server/flag_server.gd
+++ classes/global/flag_server/flag_server.gd
@@ -1,10 +1,14 @@
 extends Node
 # Singleton server for boolean flags that persist between rooms in a level.
 # This could maybe be accessed via Pipescript.
+#
+# Internally, the FlagServer keeps a dictionary of arrays, keyed by the
+# room ID (which currently equals the scene filename, although this will have
+# to be changed to make the level designer work.).
 
 
-var _assign_id: int = 0
-var _flag_dict: Dictionary = {}
+var _assign_id: int = 0 # First unused index in the current room's flag array.
+var _flag_dict: Dictionary = {} # Dictionary of flag arrays, keyed by room.
 
 
 # Creates a flag in the current room's dictionary entry, and returns its ID.
@@ -12,7 +16,9 @@ func claim_flag_id() -> int:
 	# TODO: Make this apply to room IDs, not scene paths
 	# This will be used on custom levels, which don't have scene paths
 	var room = _get_room_id()
+	
 	_add_flag(room)
+	# _add_flag increments the assign ID. Return non-incremented version.
 	return _assign_id - 1
 
 
@@ -50,8 +56,10 @@ func set_flag_state(id: int, val: bool) -> void:
 # Add a flag to a room entry in the dictionary. Create a room entry if it doesn't exist.
 func _add_flag(room: String, val: bool = false):
 	if _flag_dict.has(room):
+		# Add value to existing room array.
 		_flag_dict[room].append(val)
 	else:
+		# Room array does not exist. Create it with the value inside.
 		_flag_dict[room] = [val]
 	_assign_id += 1
 
