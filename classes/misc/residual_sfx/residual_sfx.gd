0cb0c6287465776aa4ee2e909163f1ff3628b1c0
Koopa1018 <caleblovestoytrains@gmail.com>
2022-12-03 14:01:23 -0700

    Comment residual_sfx
    ...according to what Randomcatdude told me this class's purpose is. (Based on how the current codebase uses it, his explanation pans out.)

--- classes/misc/residual_sfx/residual_sfx.gd
+++ classes/misc/residual_sfx/residual_sfx.gd
@@ -1,8 +1,12 @@
 class_name ResidualSFX
 extends AudioStreamPlayer2D
+# A standalone sound player that self-destructs when the clip ends.
+# Useful for things that need to play a sound as they're destroyed
+# (which would destroy any sound nodes contained inside the object).
 
 
 func _ready():
+	# Set self up to self-destruct once the sound is finished.
 	# warning-ignore:return_value_discarded
 	connect("finished", self, "_on_ResidualSFX_finished")
 	play()
