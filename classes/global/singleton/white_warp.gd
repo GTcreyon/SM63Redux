f72e5aa7caa9eebe0c2a74b1546706e185678ffc
Koopa1018 <caleblovestoytrains@gmail.com>
2022-12-05 11:11:39 -0700

    Rename WhiteWarp state enum to match convention
    Weird how In = less scene, out = more scene showing--opposite of fade out and fade in.

--- classes/global/singleton/white_warp.gd
+++ classes/global/singleton/white_warp.gd
@@ -1,9 +1,9 @@
 extends Polygon2D
 
 enum EnterState {
-	FADE_IN = -1,
+	FADE_OUT = -1,
 	NONE = 0,
-	FADE_OUT = 1,
+	FADE_IN = 1,
 }
 
 var in_time = 25
@@ -21,8 +21,9 @@ func _process(delta):
 	var in_unit = 1.0 / in_time
 	var out_unit = 1.0 / out_time
 	
-	if enter == EnterState.FADE_OUT:
+	if enter == EnterState.FADE_IN:
 		cover.color.a = progress + in_unit
+		print_debug(cover.color.a)
 		
 		if progress >= 1 - in_unit:
 			# Do actual warp.
@@ -33,12 +34,12 @@ func _process(delta):
 			Singleton.warp_to(scene_path)
 			
 			# Prepare to fade in on the other side.
-			enter = EnterState.FADE_IN
+			enter = EnterState.FADE_OUT
 			progress = 0
 		else:
 			# Tick progress timer.
 			progress += in_unit * dmod
-	elif enter == EnterState.FADE_IN:
+	elif enter == EnterState.FADE_OUT:
 		cover.color.a = 1.0 - progress
 		
 		if progress >= 1.0:
@@ -61,7 +62,7 @@ func warp(location: Vector2, path: String, t_in = 25, t_out = 15):
 	in_time = t_in
 	out_time = t_out
 	
-	enter = EnterState.FADE_OUT
+	enter = EnterState.FADE_IN
 	visible = true
 	cover.visible = visible
 	
