27697b175fd659bc9129546614ede8cd5cd417fd
Koopa1018 <caleblovestoytrains@gmail.com>
2023-01-06 16:04:24 -0700

    Conform to style guide (and make self-consistent)
    No code changes, just renaming and reorganizing things to both match the style guide (internal fns before interface, private things start with an underscore) and be self-consistent (are values saved from last frame called "was" or "prev"?).

--- gui/pause/options/options_menu.gd
+++ gui/pause/options/options_menu.gd
@@ -2,76 +2,69 @@ extends Control
 
 const SCROLL_SPEED = 16
 
-onready var list = $List
-onready var camera_fix = $List/CameraFix
-onready var touch_controls = $List/TouchControls
-onready var mute_music = $List/MuteMusic
-onready var mute_sfx = $List/MuteSFX
-onready var show_timer = $List/ShowTimer
-onready var locale_select = $List/LocaleSelect
-onready var button_menu = $List/ButtonMenu
-onready var touch_menu = $List/TouchMenu
-var all_interactables = []
-var bus_music = AudioServer.get_bus_index("Music")
-var bus_sfx = AudioServer.get_bus_index("SFX")
-var height_set = false
-var start_height
-var max_height
-var was_visible = false
-var was_paused = false
+var _all_interactables = []
+var _bus_music = AudioServer.get_bus_index("Music")
+var _bus_sfx = AudioServer.get_bus_index("SFX")
+var _height_set = false
+var _start_height
+var _max_height
+var _prev_visible = false
+var _prev_paused = false
+var _prev_scale = 1
+
+onready var _root_list = $List
+onready var _camera_fix = $List/CameraFix
+onready var _touch_controls = $List/TouchControls
+onready var _mute_music = $List/MuteMusic
+onready var _mute_sfx = $List/MuteSFX
+onready var _show_timer = $List/ShowTimer
+onready var _locale_select = $List/LocaleSelect
+onready var _button_menu = $List/ButtonMenu
+onready var _touch_menu = $List/TouchMenu
 
 
 func _ready():
 	_reset_values()
-	start_height = rect_size.y - list.margin_top + list.margin_bottom
-	height_set = true
+	_start_height = rect_size.y - _root_list.margin_top + _root_list.margin_bottom
+	_height_set = true
 	
 	# Save a list of all interactable child controls for enabling+disabling.
 	# (Best not run this every frame. Once-and-cache is more efficient.)
-	all_interactables = get_all_interactable_controls(self)
-
-
-func _reset_values():
-	camera_fix.pressed = Singleton.disable_limits
-	touch_controls.pressed = Singleton.touch_control
-	mute_music.pressed = AudioServer.is_bus_mute(bus_music)
-	mute_sfx.pressed = AudioServer.is_bus_mute(bus_sfx)
-	show_timer.pressed = Singleton.timer.visible
-	$List/CameraFix/Sprite.playing = camera_fix.pressed
-	$List/TouchControls/Sprite.playing = touch_controls.pressed
-	$List/MuteMusic/Sprite.playing = mute_music.pressed
-	$List/MuteSFX/Sprite.playing = mute_sfx.pressed
-	$List/ShowTimer/Sprite.playing = show_timer.pressed
+	_all_interactables = _get_all_interactable_controls(self)
 
 
 func _process(_delta):
-	manage_sizes()
-	max_height = list.rect_size.y
+	_manage_sizes()
+	_max_height = _root_list.rect_size.y
 	
 	if visible:
-		# If we just became visible, initalize and re-enable the menu.
-		if !was_visible:
-			# Reload current settings.
+		if !_prev_visible:
+			# We just became visible. Reload current settings.
 			_reset_values()
 		
 		# Set the actual settings.
-		Singleton.disable_limits = camera_fix.pressed
-		Singleton.touch_control = touch_controls.pressed
-		AudioServer.set_bus_mute(bus_music, mute_music.pressed)
-		AudioServer.set_bus_mute(bus_sfx, mute_sfx.pressed)
-		Singleton.timer.visible = show_timer.pressed
-		button_menu.visible = !Singleton.touch_control
-		touch_menu.visible = Singleton.touch_control
+		Singleton.disable_limits = _camera_fix.pressed
+		Singleton.touch_control = _touch_controls.pressed
+		AudioServer.set_bus_mute(_bus_music, _mute_music.pressed)
+		AudioServer.set_bus_mute(_bus_sfx, _mute_sfx.pressed)
+		Singleton.timer.visible = _show_timer.pressed
+		_button_menu.visible = !Singleton.touch_control
+		_touch_menu.visible = Singleton.touch_control
 		
-	if Singleton.pause_menu and !was_paused:
+	if Singleton.pause_menu and !_prev_paused:
 		# Just became paused.
-		enable_all_interactables()
-	elif !Singleton.pause_menu and was_paused:
+		_enable_all_interactables()
+	elif !Singleton.pause_menu and _prev_paused:
 		# Just unpaused.
-		disable_all_interactables()
+		_disable_all_interactables()
 	
-	was_visible = visible
-	was_paused = Singleton.pause_menu
+	_prev_visible = visible
+	_prev_paused = Singleton.pause_menu
+
+
+func _notification(what):
+	if what == NOTIFICATION_RESIZED and _height_set:
+		_start_height = rect_size.y - _root_list.margin_top + _root_list.margin_bottom
 
 
 func _on_OptionsMenu_gui_input(event):
@@ -79,42 +72,50 @@ func _on_OptionsMenu_gui_input(event):
 	# Read scroll wheel events.
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_WHEEL_DOWN:
-			list.margin_top = max(list.margin_top - SCROLL_SPEED * scale, -(max_height - start_height) + list.margin_left)
-			list.margin_bottom = list.margin_top - 16
+			_root_list.margin_top = max(_root_list.margin_top - SCROLL_SPEED * scale,
+					-(_max_height - _start_height) + _root_list.margin_left)
+			_root_list.margin_bottom = _root_list.margin_top - 16
 		elif event.button_index == BUTTON_WHEEL_UP:
-			list.margin_top = min(list.margin_top + SCROLL_SPEED * scale, 8)
-			list.margin_bottom = list.margin_top - 16
+			_root_list.margin_top = min(_root_list.margin_top + SCROLL_SPEED * scale, 8)
+			_root_list.margin_bottom = _root_list.margin_top - 16
 
 
-func _notification(what):
-	if what == NOTIFICATION_RESIZED and height_set:
-		start_height = rect_size.y - list.margin_top + list.margin_bottom
+func _reset_values():
+	_camera_fix.pressed = Singleton.disable_limits
+	_touch_controls.pressed = Singleton.touch_control
+	_mute_music.pressed = AudioServer.is_bus_mute(_bus_music)
+	_mute_sfx.pressed = AudioServer.is_bus_mute(_bus_sfx)
+	_show_timer.pressed = Singleton.timer.visible
+	$List/CameraFix/Sprite.playing = _camera_fix.pressed
+	$List/TouchControls/Sprite.playing = _touch_controls.pressed
+	$List/MuteMusic/Sprite.playing = _mute_music.pressed
+	$List/MuteSFX/Sprite.playing = _mute_sfx.pressed
+	$List/ShowTimer/Sprite.playing = _show_timer.pressed
 
 
-var prev_scale = 1
-func manage_sizes():
+func _manage_sizes():
 	var scale = max(floor(OS.window_size.x / Singleton.DEFAULT_SIZE.x), 1)
-	camera_fix.rect_scale = Vector2.ONE * scale
-	touch_controls.rect_scale = Vector2.ONE * scale
-	mute_music.rect_scale = Vector2.ONE * scale
-	mute_sfx.rect_scale = Vector2.ONE * scale
-	#locale_select.rect_scale = Vector2.ONE * scale
-	#locale_select.margin_right = 0
+	_camera_fix.rect_scale = Vector2.ONE * scale
+	_touch_controls.rect_scale = Vector2.ONE * scale
+	_mute_music.rect_scale = Vector2.ONE * scale
+	_mute_sfx.rect_scale = Vector2.ONE * scale
+	#_locale_select.rect_scale = Vector2.ONE * scale
+	#_locale_select.margin_right = 0
 	for node in get_tree().get_nodes_in_group("rebinds"):
 		node.scale = scale
-	if prev_scale != scale:
-		camera_fix.rect_min_size = camera_fix.rect_min_size / prev_scale * scale
-		touch_controls.rect_min_size = touch_controls.rect_min_size / prev_scale * scale
-		mute_music.rect_min_size = mute_music.rect_min_size / prev_scale * scale
-		mute_sfx.rect_min_size = mute_sfx.rect_min_size / prev_scale * scale
-		#locale_select.rect_min_size.y = 32# * scaleZz
+	if _prev_scale != scale:
+		_camera_fix.rect_min_size = _camera_fix.rect_min_size / _prev_scale * scale
+		_touch_controls.rect_min_size = _touch_controls.rect_min_size / _prev_scale * scale
+		_mute_music.rect_min_size = _mute_music.rect_min_size / _prev_scale * scale
+		_mute_sfx.rect_min_size = _mute_sfx.rect_min_size / _prev_scale * scale
+		#_locale_select.rect_min_size.y = 32# * scaleZz
 		for node in get_tree().get_nodes_in_group("rebinds"):
-			node.rect_min_size.y = node.rect_min_size.y / prev_scale * scale
+			node.rect_min_size.y = node.rect_min_size.y / _prev_scale * scale
 		
-		prev_scale = scale
+		_prev_scale = scale
 
 
-func get_all_interactable_controls(this: Node):
+func _get_all_interactable_controls(this: Node):
 	var returner = []
 	
 	for node in this.get_children():
@@ -126,19 +127,19 @@ func get_all_interactable_controls(this: Node):
 		# If this node has children, search those for buttons.
 		# TODO: probably faster without recursion. How to do that?
 		elif node.get_child_count() > 0:
-			returner.append_array(get_all_interactable_controls(node))
+			returner.append_array(_get_all_interactable_controls(node))
 	
 	return returner
 
 
-func enable_all_interactables():
+func _enable_all_interactables():
 	# Set all controls to take mouse events.
 	# ("STOP" here means stop parent nodes from also taking the events.)
-	for control in all_interactables:
+	for control in _all_interactables:
 		control.mouse_filter = MOUSE_FILTER_STOP
 
 
-func disable_all_interactables():
+func _disable_all_interactables():
 	# Set all controls to ignore mouse events.
-	for control in all_interactables:
+	for control in _all_interactables:
 		control.mouse_filter = MOUSE_FILTER_IGNORE
