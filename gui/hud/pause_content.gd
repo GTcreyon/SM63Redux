1550b60a12d7f54585fdbf7560181c955522f5ef
Koopa1018 <caleblovestoytrains@gmail.com>
2023-01-06 08:01:25 -0700

    Condense and comment pause content script
    Nothing broke, so it seems this code is logically equivalent to what was there before. I just hope this is similarly understandable....

--- gui/hud/pause_content.gd
+++ gui/hud/pause_content.gd
@@ -13,40 +13,19 @@ onready var exit_button = get_parent().get_node("ButtonExit")
 
 func _process(_delta):
 	# Control nodes don't like to be made invisible then visible in one frame, it messes with input
+	
+	# Show whichever screen's button is pressed
+	# (The script for the buttons guarantees only one can be pressed at a time)
+	map.visible = map_button.pressed
+	fludd.visible = fludd_button.pressed
+	options.visible = options_button.pressed
+	exit.visible = exit_button.pressed
+	# If no button is pressed, show the info screen
+	info.visible = !map.visible and !fludd.visible and !options.visible and !exit.visible
+	
+	# Show in-game stats if unpaused OR on the info screen
 	for node in stats:
-		node.visible = !Singleton.pause_menu
-	if map_button.pressed:
-		map.visible = true
-		fludd.visible = false
-		options.visible = false
-		exit.visible = false
-		info.visible = false
-	elif fludd_button.pressed:
-		map.visible = false
-		fludd.visible = true
-		options.visible = false
-		exit.visible = false
-		info.visible = false
-	elif options_button.pressed:
-		map.visible = false
-		fludd.visible = false
-		options.visible = true
-		exit.visible = false
-		info.visible = false
-	elif exit_button.pressed:
-		map.visible = false
-		fludd.visible = false
-		options.visible = false
-		exit.visible = true
-		info.visible = false
-	else:
-		map.visible = false
-		fludd.visible = false
-		options.visible = false
-		exit.visible = false
-		info.visible = true
-		for node in stats:
-			node.visible = true
+		node.visible = !Singleton.pause_menu or info.visible
 
 
 func resize(scale):
@@ -57,3 +36,5 @@ func resize(scale):
 	margin_bottom = -33 * scale - (OS.window_size.y / scale - 52) * (scale - 1)
 	info.resize(scale)
 	map.resize(scale)
+	# TODO: Presumably the FLUDD and Exit screens will need resizing eventually
+	# (maybe not Options though?)
