1cd7ad7d276536b01f5f7b9a6089d665ef4efbdc
Koopa1018 <caleblovestoytrains@gmail.com>
2023-01-19 18:59:47 -0700

    Comment dejitter_group.gd
    Creyon requested some clarity on my earlier changes, so I provided that...
    ...and it looked a little bare with just the `dejitter_position` lines commented, so I did the whole thing.
    
    I'm *pretty* sure this is accurate. Glad I have reviewers to double check.

--- classes/dejitter_group/dejitter_group.gd
+++ classes/dejitter_group/dejitter_group.gd
@@ -1,15 +1,22 @@
 class_name DejitterGroup, "./dejitter_group.svg"
 extends Node2D
+# DejitterGroup ensures that when a visible object is stationary,
+# its pixels are always exactly aligned with world pixels.
 
+# DejitterGroup overwrites the object's position.
+# This variable exists so the objects in the group can still have
+# their positions adjusted from code.
 export var dejitter_position = Vector2.ZERO
 
 onready var parent = get_parent()
 var last_pos : Vector2
 
 func _physics_process(_delta):
+	# Find offset needed to fit uneven-sized sprites to the grid
 	var offset_vec : Vector2
 	var carrier_node = get_node(get_path()) # Get the current node
 	if carrier_node is AnimatedSprite:
+		# Set offset relative to current frame of animation
 		var frames = carrier_node.frames
 		var frame_num = carrier_node.frame
 		var animation = carrier_node.animation
@@ -17,13 +24,19 @@ func _physics_process(_delta):
 		offset_vec = frames.get_frame(animation, frame_num).get_size().posmod(2) / 2
 	else:
 		if carrier_node.get("texture") == null:
+			# No texture, so no offset needed
 			offset_vec = Vector2.ZERO
 		else:
+			# Set offset relative to entire texture
 			offset_vec = carrier_node.texture.get_size().posmod(2) / 2
+	# Apply user-defined offset on top of uneven-size offset
 	offset_vec += dejitter_position
 	
+	# If parent hasn't moved, place this object on the world grid
 	if parent.global_position.x == last_pos.x:
 		global_position.x = round(parent.global_position.x) + offset_vec.x
 	if parent.global_position.y == last_pos.y:
 		global_position.y = round(parent.global_position.y) + offset_vec.y
+	
+	# Save parent's position to check next frame
 	last_pos = parent.global_position
