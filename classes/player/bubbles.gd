9217a3048516f027b4519635cdb83b733fce5960
Koopa1018 <caleblovestoytrains@gmail.com>
2023-01-25 19:26:53 -0700

    Reorganize to standard
    Builtins before self-writtens.

--- classes/player/bubbles.gd
+++ classes/player/bubbles.gd
@@ -3,15 +3,6 @@ extends Sprite
 onready var viewport = $"../BubbleViewport"
 onready var cam = $"/root/Main/Player/Camera"
 
-func refresh():
-	# Set the viewport size to the window size
-	viewport.size = OS.window_size
-	# Create a texture for the bubbles
-	var tex = ImageTexture.new()
-	tex.create(viewport.size.x, viewport.size.y, Image.FORMAT_RGB8)
-	texture = tex
-	# Now give the shader our viewport texture
-	material.set_shader_param("viewport_texture", viewport.get_texture())
 
 func _ready():
 	refresh()
@@ -22,6 +13,7 @@ func _ready():
 	$"/root/Main/Player".call_deferred("remove_child", viewport)
 	$"/root/Main".call_deferred("add_child", viewport)
 
+
 func _process(_delta):
 	viewport.canvas_transform = get_canvas_transform()
 	# Set the position to the screen center
@@ -29,3 +21,14 @@ func _process(_delta):
 	position = (viewport.size / 2 - cam.get_canvas_transform().origin) * scale
 	# Update shader pixel scale so the bubble outline is independent of viewport res
 	material.set_shader_param("zoom", cam.zoom.x * 1.5 )
+
+
+func refresh():
+	# Set the viewport size to the window size
+	viewport.size = OS.window_size
+	# Create a new texture for self
+	var tex = ImageTexture.new()
+	tex.create(viewport.size.x, viewport.size.y, Image.FORMAT_RGB8)
+	texture = tex
+	# Now give the shader our viewport texture
+	material.set_shader_param("viewport_texture", viewport.get_texture())
