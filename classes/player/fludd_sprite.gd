b1ab09929fd4a9de1678baf3cdce6a94f733ad7c
Koopa1018 <caleblovestoytrains@gmail.com>
2023-03-02 18:58:18 -0700

    Add easy-author orientation tables
    ...for each player animation. I figure this design will be easier to author than hardcoding behavior for every animation.
    
    This is the first place the new spin animation's rotations are written.

--- classes/player/fludd_sprite.gd
+++ classes/player/fludd_sprite.gd
@@ -1,6 +1,52 @@
 extends AnimatedSprite
 # FLUDD pack visuals
 
+# Data about FLUDD's orientation
+enum Orient {
+	# "Meaning bits" - components of facing directions
+	X_FLIP = 1, # flip opposite player sprite, instead of with
+	SORT_ABOVE = 2,
+	SIDE = 4, # hover heads are exactly aligned, only one visible
+	FACING = 8, # hover heads are on the camera plane - used for back too
+	THREE_QTR = 4 | 8,
+	
+	# Definitions of FLUDD's placement for each player orientation
+	# For these, facing right = unflipped.
+	FRONT = 8,
+	FRONT_RIGHT = 4 | 8,
+	RIGHT = 4,
+	BACK_RIGHT = 4 | 8 | 2,
+	BACK = 8 | 2,
+	BACK_LEFT = 4 | 8 | 1 | 2,
+	LEFT = 4 | 1,
+	FRONT_LEFT = 4 | 8 | 1,
+}
+
+# FLUDD's orientation is overridden for these player animations.
+# (Default is Orient.FRONT_RIGHT)
+# Each frame should have one array entry.
+# (NOTE: No reason to include the non-FLUDD variants, since FLUDD is
+# hidden when those are in use!)
+const ORIENT_FRAMES = {
+	"back_fludd": Orient.BACK,
+	"front_fludd": Orient.FRONT,
+	"spin_fast_fludd": [ # 3 frames
+		Orient.FRONT_RIGHT,
+		Orient.FRONT,
+		Orient.FRONT_LEFT
+	],
+	"spin_slow_fludd": [ # 8 frames, starts facing back, ccw rotation
+		Orient.BACK,
+		Orient.BACK_LEFT,
+		Orient.LEFT,
+		Orient.FRONT_LEFT,
+		Orient.FRONT,
+		Orient.FRONT_RIGHT,
+		Orient.RIGHT,
+		Orient.BACK_RIGHT,
+	],
+}
+
 onready var player_sprite = $".."
 onready var player_body = $"../.."
 
