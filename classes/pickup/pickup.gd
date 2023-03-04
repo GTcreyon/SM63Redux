8677b610e3e0f118d715145d5b3f6e727e328244
Koopa1018 <caleblovestoytrains@gmail.com>
2023-03-04 04:36:43 -0700

    Add pickup respawn functionality
    I know several pickups need this. There's bugs to be fixed (touching the disabled object resets the timer), but otherwise...solid.

--- classes/pickup/pickup.gd
+++ classes/pickup/pickup.gd
@@ -7,10 +7,12 @@ export var parent_is_root: bool = false
 
 # Enables pickup ID behavior, so pickups can only be collected once per level.
 export var persistent_collect = true
+export(float, 0.0, 30.0, 1.0) var respawn_seconds = 0.0
 export var disabled: bool = false setget set_disabled
 export var _sprite_path: NodePath = "Sprite"
 
 var _pickup_id: int = -1
+var _respawn_timer: float = -1
 
 onready var sprite = get_node_or_null(_sprite_path)
 
@@ -25,6 +27,20 @@ func _ready_override() -> void:
 	if persistent_collect && _pickup_id == -1:
 		_pickup_id_setup()
 	_connect_signals()
+
+
+func _physics_process(delta):
+	if respawn_seconds > 0 and _respawn_timer != -1:
+		if _respawn_timer > respawn_seconds:
+			# Show and re-enable the object.
+			set_disabled(false)
+			if sprite != null:
+				sprite.visible = true
+			# Unset the timer.
+			_respawn_timer = -1
+		else:
+			# Tick the respawn timer.
+			_respawn_timer += 1.0 / 60
 
 
 # Allow a pickup ID to be manually assigned.
@@ -66,10 +82,19 @@ func _pickup_id_setup() -> void:
 
 
 func _kill_pickup() -> void:
-	if parent_is_root:
-		get_parent().queue_free()
+	if respawn_seconds == 0.0:
+		# No respawn. Destroy object permanently.
+		if parent_is_root:
+			get_parent().queue_free()
+		else:
+			queue_free()
 	else:
-		queue_free()
+		# Will respawn soon. Hide and disable in the meantime.
+		set_disabled(true)
+		if sprite != null:
+			sprite.visible = false
+		# Start the respawn timer.
+		_respawn_timer = 0.0
 
 
 func _pickup_effect() -> void:
