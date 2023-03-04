87ac24aff9a2c6962d2a30736f9187b5ec0929e8
Koopa1018 <caleblovestoytrains@gmail.com>
2023-03-04 14:31:27 -0700

    Adjust menu when close button is hidden

--- gui/pause/exit/exit_menu.gd
+++ gui/pause/exit/exit_menu.gd
@@ -1,12 +1,22 @@
 extends Control
 
-onready var exit_button = $ButtonCloseGame
+onready var exit_lvl_button = $ButtonExitLevel
+onready var to_title_button = $ButtonQuitToTitle
+onready var close_game_button = $ButtonCloseGame
 
 
 func _ready():
 	# Hide exit-game button for platforms that can't use it
 	match OS.get_name():
 		"Android", "IOS", "Web":
-			exit_button.visible = false
+			_hide_close_button()
 		_:
-			exit_button.visible = true
+			close_game_button.visible = true
+
+
+func _hide_close_button():
+	close_game_button.visible = false
+	# move the other two buttons down to fill the space
+	var button_height = close_game_button.rect_size.y
+	exit_lvl_button.rect_position.y += button_height / 2
+	to_title_button.rect_position.y += button_height / 2
