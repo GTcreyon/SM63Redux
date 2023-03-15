ba5284f82bd286934f5a86ba8b069288d8905d7f
Koopa1018 <caleblovestoytrains@gmail.com>
2023-03-15 16:46:47 -0600

    Function to get animation's real time length
    This should be useful in making reusable start-and-loop code.

--- classes/player/character_sprite.gd
+++ classes/player/character_sprite.gd
@@ -65,3 +65,10 @@ func _physics_process(delta):
 						float(spin_progress - BEGIN_SLOW_SPIN_AFTER) / (parent.SPIN_TIME - BEGIN_SLOW_SPIN_AFTER))
 			parent.S.HURT:
 				animation = "hurt"
+
+
+func _anim_length_gameframes (anim_name: String) -> int:
+	var frame_count: float = self.frames.get_frame_count(anim_name)
+	var fps: float = self.frames.get_animation_speed(anim_name)
+	
+	return int((frame_count/fps) * 60)
