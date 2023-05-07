a3ffa4cf8c6d737ae5a1580d721b28dc7d2f4590
Koopa1018 <caleblovestoytrains@gmail.com>
2023-05-07 16:47:00 -0600

    Comment BG plane script
    I'm attempting to make sense of it...not quite there yet.

--- backgrounds/bg_plane.gd
+++ backgrounds/bg_plane.gd
@@ -1,17 +1,42 @@
 extends TextureRect
 
+const Y_SIZE = 343
+const MAGIC = 150
+
 onready var cam = $"/root/Main".find_node("Camera", true, false)
+var scroll = 0
+
 
 func _process(_delta):
+	# Get zoom level so we can factor that in.
 	rect_scale = Vector2.ONE * Singleton.get_screen_scale(1)
+	
+	# Ensure we have a valid camera reference.
 	if !weakref(cam).get_ref(): # DO NOT use an else statement, this has to happen sequentially
 		cam = $"/root/Main".find_node("Camera", true, false)
+	
+	# If we successfully got a valid camera reference, do parallax logic.
 	if weakref(cam).get_ref():
+		# Save dimensions of plane and camera for later.
 		var cam_pos = cam.get_camera_position()
 		var size = texture.get_size().x
-		var target = max(-343, (-343 -cam_pos.y) / 5 / rect_scale.x - 150 * rect_scale.x)
+		
+		# Plane can't actually move relative to the camera.
+		# Instead, simulate scrolling on the X axis.
 		margin_left = (fmod(-cam_pos.x / 20, size) - size) * rect_scale.x
-		if abs(margin_top - target) > 50:
-			margin_top = target
+		
+		# Do likewise for the Y axis.
+		var top_target = max(-Y_SIZE,
+			( 
+				(-Y_SIZE - cam_pos.y) / 5
+					/
+				rect_scale.x
+			)
+			-
+			MAGIC * rect_scale.x
+		)
+		if abs(margin_top - top_target) > 50:
+			# Appears to happen when the camera snaps back in bounds.
+			margin_top = top_target
 		else:
-			margin_top = lerp(margin_top, target, 0.05)
+			margin_top = lerp(margin_top, top_target, 0.05)
