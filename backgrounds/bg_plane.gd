e3bf8f38c970ad0faa4f94e568b917c2d3d42483
Koopa1018 <caleblovestoytrains@gmail.com>
2023-05-07 17:26:42 -0600

    Return the old Y-pos algorithm
    Turns out, it worked fine. In debugging, I've found some jank, though...I think I can refine that away.

--- backgrounds/bg_plane.gd
+++ backgrounds/bg_plane.gd
@@ -1,7 +1,8 @@
 extends TextureRect
 
 const Y_SIZE = 343
-const MAGIC = 150
+const Y_OFFSET = 150
+const PARALLAX_FACTOR = Vector2(20, 5)
 
 onready var cam = $"/root/Main".find_node("Camera", true, false)
 var scroll = 0
@@ -23,5 +24,17 @@ func _process(_delta):
 		
 		# Plane can't actually move relative to the camera.
 		# Instead, simulate scrolling.
-		margin_left = (fmod(-cam_pos.x / 20, size) - size) * rect_scale.x
-		margin_top = (fmod(-cam_pos.y / 20, size) - size) * rect_scale.y
+		
+		# Wrap X position to within the main width of the texture.
+		margin_left = fmod(-cam_pos.x / PARALLAX_FACTOR.x, size)
+		# Offset by the texture size.
+		margin_left -= size
+		# Counteract camera zoom so BG stays same size onscreen.
+		margin_left *= rect_scale.x
+		
+		# Place Y position at its proper height.
+		margin_top = -(Y_SIZE + cam_pos.y) / PARALLAX_FACTOR.y / rect_scale.x
+		# Offset by a user-defined amount.
+		margin_top -= Y_OFFSET * rect_scale.x
+		# Ensure the camera never crosses below the plane.
+		margin_top = max(-Y_SIZE, margin_top)
