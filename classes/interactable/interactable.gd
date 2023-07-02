81faa7dc4e08f5188517b82a90a8d113849ba429
Koopa1018 <caleblovestoytrains@gmail.com>
2023-07-02 05:43:55 -0600

    Simplify sprite line even more
    aloelucidity let me know that you can just export node types now, which is. objectively the simplest way this can work :satisfied:

--- classes/interactable/interactable.gd
+++ classes/interactable/interactable.gd
@@ -3,7 +3,7 @@ extends Area2D
 # Root class for objects that can be interacted with by entering an area and performing an action.
 
 @export var disabled: bool = false: set = set_disabled
-@export(NodePath) onready var sprite = get_node(sprite) as Node2D
+@export var sprite: Node2D
 
 
 func _physics_process(_delta):
