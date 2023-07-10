278f8a9f94cc37a2a7245244094745830dffe308
Koopa1018 <caleblovestoytrains@gmail.com>
2023-07-09 21:48:20 -0600

    Comment and cleanup water_viewport.gd
    Idk if this is helpful to anyone else, but I needed it.

--- classes/water/water_viewport.gd
+++ classes/water/water_viewport.gd
@@ -2,49 +2,62 @@
 extends Sprite2D
 
 const WATER_VIEWPORT_MATERIAL = preload("res://classes/water/water_viewport.tres")
-
-@onready var viewport: SubViewport = $SubViewport
-@onready var water: Polygon2D = $SubViewport/WaterPolygon
-@onready var detection_area: Area2D = $DetectionArea
-@onready var collision: CollisionPolygon2D = $DetectionArea/Collision
+const FRAME_TIMESTEP = 0.2
 
 @export var polygon: PackedVector2Array = PackedVector2Array()
 @export var outline_texture: Texture2D = load("res://classes/water/water_outline_anim.png")
 @export var water_texture_size: Vector2 = Vector2(64, 64)
 @export var outline_texture_size: Vector2 = Vector2(32, 12)
 @export var water_color: Color = Color(0, 0.7, 1, 0.8)
-#export var texture_color_impact: float = 0.2;
-#export var animation_swing_range: float = 32;
-#export var animation_speed: float = 1;
+#@export var texture_color_impact: float = 0.2;
+#@export var animation_swing_range: float = 32;
+#@export var animation_speed: float = 1;
+
+var next_frame_time = FRAME_TIMESTEP
+var frame_timer = 0
+var current_frame = 0
+
+@onready var viewport: SubViewport = $SubViewport
+@onready var water: Polygon2D = $SubViewport/WaterPolygon
+@onready var detection_area: Area2D = $DetectionArea
+@onready var collision: CollisionPolygon2D = $DetectionArea/Collision
 
 func refresh():
 	#viewport.world_2d = get_world_2d()
-	# Get the extends
+
+	# Find the axis-aligned bounding box of the water polygon.
 	var min_vec = Vector2.INF
 	var max_vec = -Vector2.INF
-	for poly in polygon:
-		min_vec.x = min(min_vec.x, poly.x)
-		min_vec.y = min(min_vec.y, poly.y)
-		max_vec.x = max(max_vec.x, poly.x)
-		max_vec.y = max(max_vec.y, poly.y)
+	for vertex in polygon:
+		min_vec.x = min(min_vec.x, vertex.x)
+		min_vec.y = min(min_vec.y, vertex.y)
+		max_vec.x = max(max_vec.x, vertex.x)
+		max_vec.y = max(max_vec.y, vertex.y)
 	if min_vec == Vector2.INF: # If we have 0 polygons, then don't do anything
 		return
-	# Set the size to the extends of the polygon
-	var size_extends = max_vec - min_vec
-	var size_diff = Vector2(0, size_extends.y * 1.5)
-	viewport.size = size_extends + size_diff
-	# Set the water polygon and start it
-	water.position += size_diff / 2
+	
+	# Find size of the AABB.
+	var size_extents = max_vec - min_vec
+	# This will be used to add headroom to the viewport.
+	var size_diff = Vector2(0, size_extents.y * 1.5)
+	
+	# Set viewport size big enough to contain the whole polygon plus headroom.
+	viewport.size = size_extents + size_diff
+	
+	# Set up the water polygon.
+	water.position += size_diff / 2 # TODO: what happens w/o this?
 	water.polygon = polygon
-	# Now set the collision polygon
+	# Collision polygon is just a rectangle for now.
 	collision.polygon = PackedVector2Array([
-		Vector2(0, 0), Vector2(size_extends.x, 0),
-		size_extends, Vector2(0, size_extends.y)
+		Vector2(0, 0), Vector2(size_extents.x, 0),
+		size_extents, Vector2(0, size_extents.y)
 	])
-	# Handle position stuff
-	position += size_extends / 2
-	collision.position -= size_extends / 2
-	detection_area.top_left_corner = position - size_extends / 2
+
+	# Put object origin at AABB center.
+	position += size_extents / 2
+	# But not collision--leave that where it was!
+	collision.position -= size_extents / 2
+	detection_area.top_left_corner = position - size_extents / 2
 	# Start!
 	detection_area.on_ready()
 	
@@ -58,15 +71,15 @@ func refresh():
 	
 	# Shader copy time--this is mostly just so it knows the size of pixels
 	var tex = Image.create(viewport.size.x, viewport.size.y, false, Image.FORMAT_L8)
-	texture = ImageTexture.create_from_image(tex)
+	texture = viewport.get_texture()
 	
 	water.color = Color(water_color.r, water_color.g, water_color.b, 1)
 	
 	# Set the water shaders
 #	var mat = water_material.duplicate()
 #	mat.set_shader_param("base_water_color", water_color)
-#	mat.set_shader_param("texture_repeat", (size_extends / water_texture_size).y)
-#	mat.set_shader_param("water_xy_ratio", Vector2(size_extends.x / size_extends.y, 1))
+#	mat.set_shader_param("texture_repeat", (size_extents / water_texture_size).y)
+#	mat.set_shader_param("water_xy_ratio", Vector2(size_extents.x / size_extents.y, 1))
 #	mat.set_shader_param("normal_map_mult", texture_color_impact)
 #	mat.set_shader_param("animation_swing_range_px", animation_swing_range)
 #	mat.set_shader_param("animation_speed", animation_speed)
@@ -75,24 +88,31 @@ func refresh():
 
 func _draw():
 	if Engine.is_editor_hint():
+		# Full water rendering isn't available in editor.
+		# Draw solid-colored polygons instead.
 		var colors = PackedColorArray()
 		for poly in polygon:
 			colors.append(Color(0, 0.7, 1))#water_color)
 		draw_polygon(polygon, colors)
+		# Draw circles on the verts as well.
 		for poly in polygon:
 			draw_circle(poly, 3, Color(0, 1, 1))
 
-var next_frame = 0.2
-var timer = 0
-var current_frame = 0
+
 func _process(dt):
 	if Engine.is_editor_hint():
 		return
-	timer += dt
-	if timer >= next_frame:
-		next_frame = timer + 0.2
+	
+	# Tick surface animation timer.
+	frame_timer += dt
+	if frame_timer >= next_frame_time:
+		# Advance surface to next flipbook frame.
 		current_frame = (current_frame + 1) % 4
 		material.set_shader_parameter("outline_anim_phase", current_frame)
+
+		# Reset timer for next frame.
+		next_frame_time = frame_timer + FRAME_TIMESTEP
+
 
 func _ready():
 	if !Engine.is_editor_hint():
