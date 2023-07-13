460fc01b135753f4a4ce14506b47e2b6b64e7077
Koopa1018 <caleblovestoytrains@gmail.com>
2023-07-12 19:26:13 -0600

    Theme the LD's grid panes
    Grid panes use theme settings from code...make it work right.
    
    Also taking the time to create a dedicated LD theme. For now, it's just a copy of the normal game theme with grid settings added; it'll probably be updated as time goes on.
    
    LD now runs.

--- scenes/menus/level_designer/item_pane.gd
+++ scenes/menus/level_designer/item_pane.gd
@@ -1,6 +1,7 @@
 extends Control
 
 const SCROLL_SPEED = 9
+const GRID_THEME_TYPE = ""
 
 @onready var main = $"/root/Main"
 @onready var base = $ItemBlock/ItemDisplay/Back/Base
@@ -8,7 +9,7 @@ const SCROLL_SPEED = 9
 
 
 func _on_LeftBar_gui_input(event):
-	var full_height = ceil(main.items.size() / 2) * (32 + grid.get_constant("v_separation")) - grid.offset_left - base.size.y + 2
+	var full_height = ceil(main.items.size() / 2) * (32 + grid.theme.get_constant("v_separation", GRID_THEME_TYPE)) - grid.offset_left - base.size.y + 2
 	
 	var factor
 	if event.factor == 0:
--- scenes/menus/level_designer/scroller.gd
+++ scenes/menus/level_designer/scroller.gd
@@ -1,12 +1,14 @@
 extends Control
 
+const GRID_THEME_TYPE = ""
+
 @onready var main = $"/root/Main"
 @onready var base = $"/root/Main/UILayer/LDUI/ItemPane/ItemBlock/ItemDisplay/Back/Base"
-@onready var grid = $"/root/Main/UILayer/LDUI/ItemPane/ItemBlock/ItemDisplay/Back/Base/ItemGrid" # ew
+@onready var grid: GridContainer = $"/root/Main/UILayer/LDUI/ItemPane/ItemBlock/ItemDisplay/Back/Base/ItemGrid" # ew
 @onready var region = get_parent()
 
 func _process(_delta):
-	var content_height = ceil(main.items.size() / 2) * (32 + grid.get_constant("v_separation"))
+	var content_height = ceil(main.items.size() / 2) * (32 + grid.theme.get_constant("v_separation", GRID_THEME_TYPE))
 	var scroll_height = content_height - base.size.y + 2
 	var region_height = region.size.y
 	var scroll_position = -(grid.offset_top - 3) / scroll_height
