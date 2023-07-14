68f7cc05dcf19fda6b65df79035db567cac44f6c
Koopa1018 <caleblovestoytrains@gmail.com>
2023-07-14 07:52:11 -0600

    Make input_manager.gd readable again
    Simplify and expand the updated fullscreen logic. Also, break long if conditions across multiple lines to avoid going over 100 characters per line.

--- classes/global/singleton/input_manager.gd
+++ classes/global/singleton/input_manager.gd
@@ -1,12 +1,21 @@
 extends Node
 
+@onready var window = get_window()
+
 
 func _process(_delta):
 	if Input.is_action_just_pressed("fullscreen") and OS.get_name() != "HTML5":
-		get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN if (!((get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN) or (get_window().mode == Window.MODE_FULLSCREEN))) else Window.MODE_WINDOWED
-	if Input.is_action_just_pressed("screen+") and get_window().size.x + Singleton.DEFAULT_SIZE.x < DisplayServer.screen_get_size().x and get_window().size.y + Singleton.DEFAULT_SIZE.y < DisplayServer.screen_get_size().y:
-		get_window().size.x += Singleton.DEFAULT_SIZE.x
-		get_window().size.y += Singleton.DEFAULT_SIZE.y
-	if Input.is_action_just_pressed("screen-") and get_window().size.x - Singleton.DEFAULT_SIZE.x >= Singleton.DEFAULT_SIZE.x:
-		get_window().size.x -= Singleton.DEFAULT_SIZE.x
-		get_window().size.y -= Singleton.DEFAULT_SIZE.y
+		if window.mode != Window.MODE_EXCLUSIVE_FULLSCREEN and window.mode != Window.MODE_FULLSCREEN:
+			window.mode = Window.MODE_EXCLUSIVE_FULLSCREEN
+		else:
+			window.mode = Window.MODE_WINDOWED
+	
+	if Input.is_action_just_pressed("screen+") \
+		and window.size.x + Singleton.DEFAULT_SIZE.x < DisplayServer.screen_get_size().x \
+		and window.size.y + Singleton.DEFAULT_SIZE.y < DisplayServer.screen_get_size().y:
+		window.size.x += Singleton.DEFAULT_SIZE.x
+		window.size.y += Singleton.DEFAULT_SIZE.y
+	if Input.is_action_just_pressed("screen-") \
+		and window.size.x - Singleton.DEFAULT_SIZE.x >= Singleton.DEFAULT_SIZE.x:
+		window.size.x -= Singleton.DEFAULT_SIZE.x
+		window.size.y -= Singleton.DEFAULT_SIZE.y
