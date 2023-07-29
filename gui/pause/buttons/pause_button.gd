7d2937dbbbd59da142ef00c20d37d88092dbeb28
Koopa1018 <caleblovestoytrains@gmail.com>
2023-07-28 20:14:36 -0600

    Let menu button stars start+stop again
    As with the broken remap buttons earlier, pause buttons were if-ing on signal "pressed" (signal = never null = always true) instead of property "button_pressed". Results were much less catastrophic, but still incorrect.

--- gui/pause/buttons/pause_button.gd
+++ gui/pause/buttons/pause_button.gd
@@ -1,8 +1,8 @@
 extends Button
 # A button on the pause menu that selects which menu is active
 
-var scroll = 0
-var scroll_goal = 0
+var scroll: float = 0.0
+var scroll_goal: float = 0.0
 
 @onready var stars = $Stars
 @onready var text_node = $Text
@@ -22,12 +22,12 @@ func _process(delta):
 		mouse_filter = MOUSE_FILTER_STOP
 	
 	# Change which star polygon is visible
-	if pressed:
+	if button_pressed:
 		stars.texture = texture_on
 	else:
 		stars.texture = texture_off
 	
-	if pressed:
+	if button_pressed:
 		scroll = fmod((scroll + 0.01 * dmod), 1.0)
 		scroll_goal = 0
 	elif is_hovered():
@@ -40,9 +40,9 @@ func _process(delta):
 			else:
 				scroll_goal = 1
 		elif scroll_goal == 1:
-			scroll = lerp(scroll, 1, 0.02)
+			scroll = lerp(scroll, 1.0, 0.02)
 		else:
-			scroll = lerp(scroll, 1, 0.04)
+			scroll = lerp(scroll, 1.0, 0.04)
 	
 	stars.texture_offset = Vector2(-15, -10) * scroll + Vector2(0, -2)
 
