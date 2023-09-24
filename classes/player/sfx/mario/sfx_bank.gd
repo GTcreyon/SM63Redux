06f1d7e9b4ec281b0540140a1b9eebff2f01e470
AidenBeresford <aidenberes@gmail.com>
2023-09-24 18:38:31 -0500

    Add sfx bank resource

---
+++ classes/player/sfx/mario/sfx_bank.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name SFXBank
+
+@export var sound_bank : Array[AudioStreamWAV]
+@export var group_bank : Array[SFXBank] # I don't know if this will be useful yet, we shall see
