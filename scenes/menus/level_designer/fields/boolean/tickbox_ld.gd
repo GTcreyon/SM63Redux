ffde339a790b4366f5029568732e1d3e9b10275a
Koopa1018 <caleblovestoytrains@gmail.com>
2023-10-08 21:43:20 -0600

    Make tickbox feel responsive
    The existing system had all frames of the tickbox animation in a single animation, arranged so that starting the animation makes the checkmark appear. The problem is, it had to go all the way through the unchecked first frame--so it had a perceptible delay between click and visual response.
    
    The simplest fix would be to jump to frame 1 when the animation starts. But if we're putting different animation code in the "if pressed" and "else" blocks, might as well go all in and use the traditional two-different-animations system.

--- scenes/menus/level_designer/fields/boolean/tickbox_ld.gd
+++ scenes/menus/level_designer/fields/boolean/tickbox_ld.gd
@@ -1,26 +1,28 @@
 extends Control
 
+var pressed = false
+
 @onready var sprite = $Button/Sprite2D
 @onready var label = $Label
 @onready var parent_menu = $"../.."
-var pressed = false
-
 
 func _ready():
 	if pressed:
-		sprite.frame = 2
+		sprite.animation = "checked"
+		# Skip past animation bit, straight to the sustained frame.
+		sprite.frame = 1
 
 
 func _on_Button_pressed():
 	pressed = !pressed
 
 	# Do aesthetic effects in response to the button press.
-	sprite.frame = 0
 	if pressed:
 		Singleton.get_node("SFX/Confirm").play()
+		sprite.animation = "checked"
 		sprite.play()
 	else:
 		Singleton.get_node("SFX/Back").play()
-		sprite.stop()
+		sprite.animation = "unchecked"
 
 	parent_menu.on_value_changed(label.text, pressed)
