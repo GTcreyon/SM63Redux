535912ce577828b8803f15bd69d539e5fed811fe
Koopa1018 <siriusbizdness@gmail.com>
2024-02-22 23:55:41 -0500

    Make SolidMirrorable extend SolidObject
    DRYing this out.
    
    This inheritance-based design only makes sense when you consider that different objects will have different mirroring behaviors. For a complex hierarchy of offset Sprite2Ds, for example, you'd need different code than for a CollisionPolygon2D. Thus, there's not much reuse to be gained from splitting Mirrorable and SolidObject into separate base classes.

--- classes/solid/solid_object_mirrorable.gd
+++ classes/solid/solid_object_mirrorable.gd
@@ -1,14 +1,8 @@
 class_name SolidObjectMirrorable
-extends StaticBody2D
+extends SolidObject
 ## A StaticBody2D which can be both disabled or mirrored in the level designer.
 
-@export var disabled = false: set = set_disabled
 @export var mirror = false: set = set_mirror
-
-
-func set_disabled(val):
-	disabled = val
-	set_collision_layer_value(0, 0 if val else 1)
 
 
 func set_mirror(val):
