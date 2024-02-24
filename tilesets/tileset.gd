f6383bef638e200cd9db253ffcd9d23264b20a81
Koopa1018 <siriusbizdness@gmail.com>
2024-02-24 09:20:26 -0500

    Create tileset resource
    This is the barest-bones version, exporting the textures used by the terrain system.
    
    Some textures have been renamed to remove potential ambiguity (corner -> endcap, shade -> shadow, edge -> side).

---
+++ tilesets/tileset.gd
@@ -0,0 +1,10 @@
+class_name Tileset
+extends Resource
+
+@export var body: Texture2D
+@export var top: Texture2D
+@export var top_shadow: Texture2D
+@export var top_endcap: Texture2D
+@export var top_endcap_shadow: Texture2D
+@export var side: Texture2D
+@export var bottom: Texture2D
