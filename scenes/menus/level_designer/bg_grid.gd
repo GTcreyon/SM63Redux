b858bd87a7483bdcc442ad00a70e4170d5afad78
Koopa1018 <siriusbizdness@gmail.com>
2024-03-09 21:13:49 -0500

    Fix LD crashing in BG code
    Turns out that when I refactored backgrounds in #184, I removed a node which looked unnecessary, but which the LD was secretly using to darken the BG. As a result, since merging from master, the LD would crash immediately on game start.
    
    To fix the crash, let's use a plain translucent ColorRect in place of a code-based darkening system which relies on the BG's node structure. It looks identical, and this way, future BG designers don't have to worry about One Arcane Node That Can Break The Game If You Don't Include It.

--- scenes/menus/level_designer/bg_grid.gd
+++ scenes/menus/level_designer/bg_grid.gd
@@ -3,9 +3,5 @@ extends TextureRect
 @onready var camera: Camera2D = $"/root/Main/Camera"
 
 
-func _ready():
-	$"../BGParent".modulate = Color(0.7, 0.7, 0.7)
-
-
 func _process(_dt):
 	material.set_shader_parameter("camera_position", camera.position)
