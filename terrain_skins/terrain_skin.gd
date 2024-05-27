5db3093c4102f731ab0515f7fedb163ddb8a28fd
Koopa1018 <siriusbizdness@gmail.com>
2024-05-27 04:12:01 -0400

    Emit changed signal when skin is modified
    So stupid that you have to do this manually per field, but there you go. Better to be explicit than inflexible, right?

--- terrain_skins/terrain_skin.gd
+++ terrain_skins/terrain_skin.gd
@@ -1,10 +1,44 @@
 class_name TerrainSkin
 extends Resource
 
-@export var body: Texture2D
-@export var top: Texture2D
-@export var top_clip: Texture2D
-@export var top_endcap: Texture2D
-@export var top_endcap_clip: Texture2D
-@export var side: Texture2D
-@export var bottom: Texture2D
+@export var body: Texture2D:
+	set(value):
+		if value != body:
+			emit_changed()
+		body = value
+
+@export var top: Texture2D:
+	set(value):
+		if value != top:
+			emit_changed()
+		top = value
+
+@export var top_clip: Texture2D:
+	set(value):
+		if value != top_clip:
+			emit_changed()
+		top_clip = value
+
+@export var top_endcap: Texture2D:
+	set(value):
+		if value != top_endcap:
+			emit_changed()
+		top_endcap = value
+
+@export var top_endcap_clip: Texture2D:
+	set(value):
+		if value != top_endcap_clip:
+			emit_changed()
+		top_endcap_clip = value
+
+@export var side: Texture2D:
+	set(value):
+		if value != side:
+			emit_changed()
+		side = value
+
+@export var bottom: Texture2D:
+	set(value):
+		if value != bottom:
+			emit_changed()
+		bottom = value
