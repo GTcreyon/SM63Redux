a25ed6271fe3c11817dde7ccd37f3011c57d4dbb
Koopa1018 <siriusbizdness@gmail.com>
2024-06-08 14:55:28 -0400

    Only update level_info font when locale changes
    Perhaps it's not taking 11 milliseconds, but this script's process function was up there with water and entities last I checked--which, again, is pretty outrageous for a static pause menu element! Now it only updates when the language changes, not once every frame.
    
    While I'm here, refactor the script to potentially support the fancy font in other languages someday. That's really just using a match statement instead of an if, though.... (and commenting it)

--- gui/hud/level_info.gd
+++ gui/hud/level_info.gd
@@ -9,11 +9,17 @@ var perm_scale = 1
 @onready var level_name = $Divider/MainContainer/LevelNamePanel/ScaleContainer/LevelName
 
 
-func _process(_delta):
-	# Update the font used, in case locale's changed.
-	if TranslationServer.get_locale().substr(0, 2) == "en":
-		level_name.add_theme_font_override("font", RUBY)
-		level_name.uppercase = true
-	else:
-		level_name.add_theme_font_override("font", BYLIGHT)
-		level_name.uppercase = false
+func _notification(what):
+	if what == NOTIFICATION_TRANSLATION_CHANGED:
+		match TranslationServer.get_locale().substr(0, 2):
+			"en":
+				# Fancy display font has all needed characters.
+				# Use it for the level name.
+				level_name.add_theme_font_override("font", RUBY)
+				# Display font has only uppercase glyphs.
+				level_name.uppercase = true
+			_:
+				# Display font may not have all needed characters.
+				# Use the standard font.
+				level_name.add_theme_font_override("font", BYLIGHT)
+				level_name.uppercase = false
