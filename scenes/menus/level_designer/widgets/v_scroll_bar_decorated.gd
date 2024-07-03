c4b77ca1946babc2ecb4b1aebe6d111f5dc51b36
Koopa1018 <siriusbizdness@gmail.com>
2024-07-02 23:52:34 -0400

    Place scrollbar decoration at center of grabber
    AAAAUGH. I can see why y'all tried to roll your own scrollbar--without access to internal nodes, there's no reliable way to even find the grabber center apart from basically rolling your own logic anyway! This isn't even feature-complete--just enough for us to hopefully get by.

--- scenes/menus/level_designer/widgets/v_scroll_bar_decorated.gd
+++ scenes/menus/level_designer/widgets/v_scroll_bar_decorated.gd
@@ -1,6 +1,11 @@
+@tool
 class_name VScrollBarDecorated
 extends VScrollBar
 ## Scroll bar with a decoration icon at the center of the grabber.
+##
+## Please be warned that for technical reasons, the decoration's position can
+## only be consistently correct if the scrollbar theme graphics have consistent
+## size and padding across regular, focused, highlighted, and pressed states.
 
 @export var decoration: Texture2D
 
@@ -8,6 +13,11 @@ var deco_sprite: TextureRect
 var deco_tex: Texture2D
 var deco_tex_highlight: Texture2D
 var deco_tex_pressed: Texture2D
+
+# Need these to get the actual displayed bar height.
+@onready var _inc_height = theme.get_icon("increment", "VScrollBar").get_height()
+@onready var _dec_height = theme.get_icon("decrement", "VScrollBar").get_height()
+@onready var _grabber_pad = _v_margins(theme.get_stylebox("grabber", "VScrollBar"))
 
 func _ready():
 	# Read decoration textures from theme
@@ -29,5 +39,27 @@ func _ready():
 	deco_sprite.texture = deco_tex
 
 
-#func _draw():
-#	match 
+func _draw():
+	var cur_sprite = deco_tex # TODO: Select by mouse-over state?
+	
+	# Calculate height of actual background area (without inc+dec buttons or
+	# the inflexible parts of the grabber).
+	var bar_height = size.y
+	bar_height -= _inc_height
+	bar_height -= _dec_height
+	bar_height -= _grabber_pad
+	
+	# How much the bar is stretched relative to its value range.
+	var bar_stretch = bar_height / (max_value - min_value)
+	
+	var grabber_height = page * bar_stretch
+	
+	var grabber_pos = (self.value - min_value) * bar_stretch
+	grabber_pos += _dec_height
+	
+	deco_sprite.position = Vector2(0, 
+		grabber_pos - cur_sprite.get_height()/2 + grabber_height/2)
+
+
+func _v_margins(stylebox: StyleBoxTexture) -> float:
+	return stylebox.texture_margin_bottom + stylebox.texture_margin_top
