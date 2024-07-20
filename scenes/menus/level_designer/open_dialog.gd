54a87e12c84e9167d05326072991af41e99587a3
Koopa1018 <siriusbizdness@gmail.com>
2024-07-20 08:36:45 -0400

    Standardize vars around JSONSerializer calls
    Same idea as earlier. Changing the name though, because Template is. not the editor's root??? it is where items get placed ,

--- scenes/menus/level_designer/open_dialog.gd
+++ scenes/menus/level_designer/open_dialog.gd
@@ -2,7 +2,7 @@ extends FileDialog
 
 func _on_OpenDialog_file_selected(path):
 	var serializer = JSONSerializer.new()
-	var EditorRoot = get_node("/root/Main/Template")
+	var template = get_node("/root/Main/Template")
 	var file = FileAccess.open(path, FileAccess.READ)
 	
-	serializer.load_level_json(file.get_as_text(), EditorRoot)
+	serializer.load_level_json(file.get_as_text(), template)
--- scenes/menus/level_designer/save_dialog.gd
+++ scenes/menus/level_designer/save_dialog.gd
@@ -2,9 +2,9 @@ extends FileDialog
 
 func _on_SaveDialog_file_selected(path):
 	var serializer = JSONSerializer.new()
-	var EditorRoot = get_node("/root/Main/Template")
+	var template = get_node("/root/Main/Template")
 	
-	var level_json = serializer.generate_level_json(EditorRoot)
+	var level_json = serializer.generate_level_json(template)
 	var file = FileAccess.open(path, FileAccess.WRITE)
 	file.store_string(level_json)
 	file.close()
