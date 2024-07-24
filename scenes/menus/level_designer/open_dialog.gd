6b7097658175b7894c1e7e454f8d7751911021a4
Koopa1018 <siriusbizdness@gmail.com>
2024-07-24 01:11:18 -0400

    Remove pointless local var
    Replace its single usage with the get_node command.

--- scenes/menus/level_designer/open_dialog.gd
+++ scenes/menus/level_designer/open_dialog.gd
@@ -2,7 +2,6 @@ extends FileDialog
 
 func _on_OpenDialog_file_selected(path):
 	var serializer = JSONSerializer.new()
-	var main = get_node("/root/Main")
 	var file = FileAccess.open(path, FileAccess.READ)
 	
-	serializer.load_level_json(file.get_as_text(), main)
+	serializer.load_level_json(file.get_as_text(), get_node("/root/Main"))
