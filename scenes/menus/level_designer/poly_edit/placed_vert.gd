f6cb1130c6dc9eb4b5818eba1930c1c43f5649b9
Koopa1018 <siriusbizdness@gmail.com>
2024-07-26 17:38:46 -0400

    Move placed verts via drag, not click
    Keeping this commit light, and there's STILL a bit to unpack! The gist of it is, placed verts now respond to left clicks on press, so it feels like dragging and dropping. Changed their mouse icon to make that clearer, too.
    
    Implementing this required a new script for placed verts, so that right click (to delete) could still respond to release only. (Because each button is now specially handled, the button action mode is now fully ignored.)
    
    Kind of awesome that I didn't have to modify polygon_editor or polygon_draw to end drag on mouse up--it just Already Did That.

---
+++ scenes/menus/level_designer/poly_edit/placed_vert.gd
@@ -0,0 +1,31 @@
+class_name PlacedVertex
+extends TextureButton
+## Widget for editing a placed polygon-editor vertex.
+
+# Thanks Dave and myyk for the initial implementation!
+# https://stackoverflow.com/questions/66090893/how-can-i-extend-godots-button-to-distinguish-left-click-vs-right-click-events
+
+## Signal emitted only when the button is left-clicked.
+signal pressed_left
+## Signal emitted only when the button is right-clicked.
+signal pressed_right
+
+func _gui_input(event: InputEvent):
+	if event is InputEventMouseButton:
+		# Abort if the click event has moved off the button.
+		if !is_hovered():
+			return
+		
+		# We didn't abort. Emit the appropriate event for the button, assuming
+		# the click event is appropriate for the button.
+		match event.button_index:
+			MOUSE_BUTTON_LEFT:
+				# Respond to left clicks on press, for a drag-and-drop-like
+				# user experience.
+				if event.pressed:
+					pressed_left.emit()
+			MOUSE_BUTTON_RIGHT:
+				# Respond to right clicks only on release, to allow the user a
+				# moment to change their mind before vertex deletion.
+				if !event.pressed:
+					pressed_right.emit()
