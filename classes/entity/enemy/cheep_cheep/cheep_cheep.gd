24f292fead4a2ebc0f275e3a8d85a6c12c34d855
Tricia <gtcreyon@gmail.com>
2024-07-26 19:47:55 +0100

    Reorganise Cheep Cheep code

--- classes/entity/enemy/cheep_cheep/cheep_cheep.gd
+++ classes/entity/enemy/cheep_cheep/cheep_cheep.gd
@@ -1,7 +1,7 @@
 class_name CheepCheep
 extends EntityEnemy
 
-const SPEED = 32.0/60.0
+const SPEED = 0.5
 
 var follow_targets = []
 var direction = null
@@ -12,66 +12,86 @@ var rng = RandomNumberGenerator.new()
 
 func _physics_step():
 	if struck:
-		vel *= 0.9
-		rotation += vel.length() * sign(vel.x) * 0.1
-		if round(vel.length()) == 0:
-			enemy_die()
+		_struck_spin()
 	else:
 		if _water_bodies > 0:
-			if follow_targets.size() > 0:
-				if sprite.animation == "idle":
-					sprite.play("notice")
-				var target = follow_targets[0]
-				direction = target.position - position
-				vel = position.direction_to(target.position) * SPEED
-				if target.position.x < position.x:
-					if !mirror:
-						rotation = -rotation
-						mirror = true
-					rotation = lerp_angle(rotation, direction.angle() - PI, 0.25)
-				else:
-					if mirror:
-						rotation = -rotation
-						mirror = false
-					rotation = lerp_angle(rotation, direction.angle(), 0.25)
-			else:
-				if sprite.animation == "chase":
-					sprite.play("calmdown")
-				vel.y *= 0.85
-				sprite.flip_v = false
-				if abs(PI - rotation) > abs(rotation):
-					rotation = lerp_angle(rotation, 0, 0.25)
-				else:
-					rotation = lerp_angle(rotation, 0, 0.25)
-				idle_time += 1
-				if idle_time > 240:
-					vel.x = max(vel.x - 0.025, -0.25)
-					mirror = true
-					if idle_time >= 480:
-						idle_time = 0
-				else:
-					mirror = false
-					vel.x = min(vel.x + 0.025, 0.25)
-					
-				if is_on_wall():
-					if vel.x > 0:
-						idle_time = 241
-					else:
-						idle_time = 0
-					vel.x = -sign(vel.x)
+			_swim()
 		else:
-			sprite.play("idle")
-			vel.y = min(vel.y + 0.25, 5)
-			if is_on_floor():
-				vel.y = -2
-				vel.x = rng.randf() * 2 - 1
-			rotation = lerp_angle(rotation, 0, 0.25)
-			mirror = vel.x < 0
+			_flounder()
 	
 	# warning-ignore:RETURN_VALUE_DISCARDED
 	set_velocity(vel * 60)
 	set_up_direction(Vector2.UP)
 	move_and_slide()
+
+
+func _flounder():
+	sprite.play("idle")
+	vel.y = min(vel.y + 0.25, 5)
+	if is_on_floor():
+		vel.y = -2
+		vel.x = rng.randf() * 2 - 1
+	rotation = lerp_angle(rotation, 0, 0.25)
+	mirror = vel.x < 0
+
+
+func _swim():
+	if follow_targets.size() > 0:
+		_pursue()
+	else:
+		_wander()
+
+
+func _wander():
+	if sprite.animation == "chase":
+		sprite.play("calmdown")
+	vel.y *= 0.85
+	sprite.flip_v = false
+	if abs(PI - rotation) > abs(rotation):
+		rotation = lerp_angle(rotation, 0, 0.25)
+	else:
+		rotation = lerp_angle(rotation, 0, 0.25)
+	idle_time += 1
+	if idle_time > 240:
+		vel.x = max(vel.x - 0.025, -0.25)
+		mirror = true
+		if idle_time >= 480:
+			idle_time = 0
+	else:
+		mirror = false
+		vel.x = min(vel.x + 0.025, 0.25)
+		
+	if is_on_wall():
+		if vel.x > 0:
+			idle_time = 241
+		else:
+			idle_time = 0
+		vel.x = -sign(vel.x)
+
+
+func _pursue():
+	if sprite.animation == "idle":
+		sprite.play("notice")
+	var target = follow_targets[0]
+	direction = target.position - position
+	vel = position.direction_to(target.position) * SPEED
+	if target.position.x < position.x:
+		if !mirror:
+			rotation = -rotation
+			mirror = true
+		rotation = lerp_angle(rotation, direction.angle() - PI, 0.25)
+	else:
+		if mirror:
+			rotation = -rotation
+			mirror = false
+		rotation = lerp_angle(rotation, direction.angle(), 0.25)
+
+
+func _struck_spin():
+	vel *= 0.9
+	rotation += vel.length() * sign(vel.x) * 0.1
+	if round(vel.length()) == 0:
+		enemy_die()
 
 
 func _on_AlertArea_body_entered(body):
@@ -87,7 +107,7 @@ func _on_AlertArea_body_exited(body):
 
 
 func _on_WaterCheck_area_exited(_area):
-	super._on_WaterCheck_area_exited(_area)
+	super(_area)
 	vel.y = -5.0
 
 
