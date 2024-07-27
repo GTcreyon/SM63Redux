f4fb2c2699bf8d1d4f40595a035cc33b348956e4
Koopa1018 <siriusbizdness@gmail.com>
2024-07-27 19:28:42 -0400

    Re-add rising during the pound spin
    This is probably the simplest part of the pound animation to integrate, since it requires minimal changes to everything else.
    
    If I wasn't totally sold on the state machine before, I think I might be now. Having all the pound-spin state encapsulated in one node? SUPER nice.

--- classes/player/state/pound_spin.gd
+++ classes/player/state/pound_spin.gd
@@ -3,6 +3,11 @@ extends PlayerState
 const FULL_DURATION: float = 18
 const SPIN_DURATION: float = 9
 
+## How many frames the player will be rising during the pound spin.
+const RISE_TIME: float = 15
+## How much the player rises each frame.
+const RISE_RATE: float = 1
+
 var progress: float
 
 
@@ -14,8 +19,16 @@ func _on_enter(_h):
 
 func _all_ticks():
 	progress += 1.0
+	
+	# Rotation animation.
 	if progress <= SPIN_DURATION:
 		motion.set_rotation(ease(progress / SPIN_DURATION, 0.5) * TAU * motion.get_facing())
+	
+	# A little rising during the wind-up makes it look real nice.
+	# This feels best if it affects the literal, physical position of the player
+	# (so that when they appear to rise above a platform, they actually do).
+	if progress <= RISE_TIME:
+		motion.set_vel_component(Vector2.DOWN, -RISE_RATE)
 
 
 func _trans_rules():
