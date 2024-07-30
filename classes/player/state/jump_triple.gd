5e716f3dfbb83f300b97fa53099ede9ce102ce32
Koopa1018 <siriusbizdness@gmail.com>
2024-07-30 00:09:59 -0400

    Don't change offset for first 3 trijump frames
    Doing this actually looks pretty bad....

--- classes/player/state/jump_triple.gd
+++ classes/player/state/jump_triple.gd
@@ -1,7 +1,6 @@
 extends JumpFlip
 
 
-const OFFSET_START = Vector2(-2, -4)
 const OFFSET = Vector2(1, -2)
 const OFFSET_FLUDD = Vector2(-1, -6)
 
@@ -9,15 +8,11 @@ const OFFSET_FLUDD = Vector2(-1, -6)
 func _all_ticks():
 	super()
 	# Update sprite's rotation origin for a nicer looking flip.
-	if _flip_frames > 3:
-		# TODO: Different origin if wearing FLUDD, for visual balance.
-		#if current_nozzle == Singleton.Nozzles.NONE:
-			actor.sprite.set_rotation_origin(OFFSET)
-		#else:
+	# TODO: Different origin if wearing FLUDD, for visual balance.
+	#if current_nozzle == Singleton.Nozzles.NONE:
+	actor.sprite.set_rotation_origin(OFFSET)
+	#else:
 		#actor.sprite.set_rotation_origin(OFFSET_FLUDD)
-	else:
-		# Use a slightly different rotation origin in the first 3 frames.
-		actor.sprite.set_rotation_origin(OFFSET_START)
 
 
 func _on_exit():
